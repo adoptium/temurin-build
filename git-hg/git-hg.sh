4722521630f72a7a7bb368c0e65fd32b12af00a0
Martijn Verburg <martijnverburg@gmail.com>
2017-03-28 20:47:44 +0100

    fix poor merge

--- git-hg/git-hg.sh
+++ git-hg/git-hg.sh
@@ -14,33 +14,12 @@
 #
 
 git clone https://github.com/frej/fast-export.git
-<<<<<<< HEAD
-git clone https://github.com/breadbin/openjdk-jdk8u.git
-cd openjdk-jdk8u
-# get latest github tag
-gitTag=`git describe --abbrev=0 --tags`
-
-cd $WORKSPACE
-rm -rf openjdk-jdk8u
-
-hg clone http://hg.openjdk.java.net/jdk8u/jdk8u/ openjdk_hg
-=======
 hg clone http://hg.openjdk.java.net/jdk8u/jdk8u/ openjdk_hg
 
->>>>>>> 4a0eb33bcca15eb3d0516858bbe6b52e8cfc8d49
 cd openjdk_hg
 # get latest mercurial tag
 hgTag=`hg log -r "." --template "{latesttag}\n"`
 
-<<<<<<< HEAD
-# compare git tag and hg tag
-if [ "$gitTag" != "$hgTag" ]; then
-	echo "Up to date"
-    exit
-fi
-
-=======
->>>>>>> 4a0eb33bcca15eb3d0516858bbe6b52e8cfc8d49
 bash ./get_source.sh
 for i in corba jaxp jaxws langtools jdk hotspot nashorn; do
    echo "cleaning up $i"
@@ -65,13 +44,6 @@ git checkout
 rm -rf .hg .hgignore .hgtags README-builds.html README get_source.sh
 # create new README
 cp $WORKSPACE/openjdk-build/git-hg/README.md .
-<<<<<<< HEAD
-git add .
-git commit -m "update source"
-git remote add releases git@github.com:AdoptOpenJDK/openjdk-jdk8u.git
-git fetch --all
-git diff releases/master
-=======
 echo "/build" > .gitignore
 git add .
 git commit -m "merge sources"
@@ -85,4 +57,3 @@ if [ `git diff releases/master | wc -l` -gt 0 ]; then
 else
 	echo "already up to date"
 fi
->>>>>>> 4a0eb33bcca15eb3d0516858bbe6b52e8cfc8d49
