cd37af833081215d7b9017b15771f7efebf5883b
Tim Ellison <t.p.ellison@gmail.com>
2017-04-03 13:52:25 +0100

    Added headers

--- jenkins/utils/shellutils.py
+++ jenkins/utils/shellutils.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python2
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 from __future__ import print_function
 import os
@@ -28,7 +23,6 @@ if sys.version_info >= (2, 7):
     subprocess_check_output = subprocess.check_output
     subprocess_check_call = subprocess.check_call
 else:
-    # SPARK-8763
     # backported from subprocess module in Python 2.7
     def subprocess_check_output(*popenargs, **kwargs):
         if 'stdout' in kwargs:
