cd7f6bb4d1c237d3acc8a9d93f75e22b25314916
George Adams <george.adams@uk.ibm.com>
2017-04-04 11:21:54 +0100

    Update git-hg.sh

--- git-hg/git-hg.sh
+++ git-hg/git-hg.sh
@@ -51,7 +51,7 @@ git remote add releases git@github.com:AdoptOpenJDK/openjdk-jdk8u.git
 git fetch --all
 # check if git diff is the same
 if [ `git diff releases/master | wc -l` -gt 0 ]; then
-	git rebase -s theirs releases/master
+	git rebase -s ours releases/master
 	git push --set-upstream releases master
 else
 	echo "already up to date"
