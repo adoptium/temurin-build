ca0f17adde96358f3e5e6ab8328fe641f16b9ac6
George Adams <george.adams@uk.ibm.com>
2017-04-04 13:59:19 +0100

    Update git-hg.sh

--- git-hg/git-hg.sh
+++ git-hg/git-hg.sh
@@ -56,6 +56,10 @@ if [ `git diff releases/master | wc -l` -gt 0 ]; then
 	git reset --hard releases/master
 	git cherry-pick --strategy=recursive -X theirs staging
 	git push --set-upstream releases master
+	# update dev branch
+	git checkout releases/dev
+	git cherry-pick --strategy=recursive -X theirs staging
+	git push --set-upstream releases dev
 else
 	echo "already up to date"
 fi
