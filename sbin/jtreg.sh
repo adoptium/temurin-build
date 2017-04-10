31dffac7dc7f0600cc8bb58de8237eb20863fb9e
neomatrix369 <sadhak001@gmail.com>
2017-04-10 16:35:03 +0100

    Extracted the dependency file name string for JTReg into a variable, controllable via an environment variable in the jtreg.sh script

--- sbin/jtreg.sh
+++ sbin/jtreg.sh
@@ -17,6 +17,8 @@ WORKING_DIR=$1
 OPENJDK_REPO_NAME=$2
 BUILD_FULL_NAME=$3
 JTREG_TEST_SUBSETS=$(echo "$4" | sed 's/:/ /')
+JTREG_BINARY_FILE=${JTREG_BINARY_FILE:-jtreg-4.2.0-tip.tar.gz}
+JTREG_TARGET_FOLDER=${JTREG_TARGET_FOLDER:-jtreg}
 
 checkIfDockerIsUsedForBuildingOrNot()
 {
@@ -32,11 +34,7 @@ checkIfDockerIsUsedForBuildingOrNot()
 downloadJtregAndSetupEnvironment() 
 {
   # Download then add jtreg to our path
-
-  JTREG_BINARY_FILE=jtreg-4.2.0-tip.tar.gz
-  JTREG_TARGET_FOLDER=jtreg
-
-  if [[ ! -d $WORKING_DIR/jtreg ]]; then
+  if [[ ! -d "${WORKING_DIR}/${JTREG_TARGET_FOLDER}" ]]; then
    echo "Downloading Jtreg binary"
    wget https://ci.adoptopenjdk.net/job/jtreg/lastSuccessfulBuild/artifact/$JTREG_BINARY_FILE
 
