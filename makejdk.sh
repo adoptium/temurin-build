158e04ac0116c9f97637cd2e7e4d5f87c4972abf
neomatrix369 <sadhak001@gmail.com>
2017-04-12 17:29:56 +0100

    makejdk.sh: replaced existing use of $VAR, ${VAR} and "${VAR}" with the respective versions depending on where the VAR was placed - use "$VAR" in if conditions - use ${VAR} is it was part of another string/variable i.e. "The source is ${VAR}" - use $VAR in all other circumstances
    Note: ${VAR} is same as "$VAR", hence "${VAR}" is a bit overloaded

--- makejdk.sh
+++ makejdk.sh
@@ -38,7 +38,7 @@ fi
 
 BUILD_TYPE=normal
 
-BUILD_FULL_NAME=$OS_KERNAL_NAME-$OS_MACHINE-$BUILD_TYPE-$JVM_VARIANT-release
+BUILD_FULL_NAME=${OS_KERNAL_NAME}-${OS_MACHINE}-${BUILD_TYPE}-${JVM_VARIANT}-release
 
 USE_DOCKER=false
 WORKING_DIR=""
@@ -106,17 +106,17 @@ parseCommandLineArgs()
 checkIfDockerIsUsedForBuildingOrNot()
 {
   # Both a working directory and a target directory provided
-  if [ ! -z "${WORKING_DIR}" ] && [ ! -z "${TARGET_DIR}" ] ; then
+  if [ ! -z "$WORKING_DIR" ] && [ ! -z "$TARGET_DIR" ] ; then
     # This uses sbin/build.sh directly
     echo "${info} Not using Docker, working area will be ${WORKING_DIR}, target for the JDK will be ${TARGET_DIR} ${normal}"
   fi
 
   # No working directory and no target directory provided
-  if [ -z "${WORKING_DIR}" ] && [ -z "${TARGET_DIR}" ] ; then
+  if [ -z "$WORKING_DIR" ] && [ -z "$TARGET_DIR" ] ; then
     echo "${info}No parameters provided, using Docker ${normal}"
     USE_DOCKER=true
-  elif [ ! -z "${TARGET_DIR}" ] && [ -z "${WORKING_DIR}" ] ; then
-    # Target dir is defined but the working dir isn't
+  elif [ ! -z "$TARGET_DIR" ] && [ -z "$WORKING_DIR" ] ; then
+    # Target directory is defined but the working directory isn't
     # Calls sbin/build.sh inside of Docker followed by a docker cp command
     echo "${info}Using Docker, target directory for the tgz on the host: ${TARGET_DIR}"
     USE_DOCKER=true
@@ -126,17 +126,17 @@ checkIfDockerIsUsedForBuildingOrNot()
 checkInCaseOfDockerShouldTheContainerBePreserved()
 {
   echo ${info}
-  if [ "${KEEP}" == "true" ] ; then
+  if [ "$KEEP" == "true" ] ; then
     echo "We'll keep the built Docker container if you're using Docker"
   else
     echo "We'll remove the built Docker container if you're using Docker"
   fi
-  echo $normal
+  echo ${normal}
 }
 
 setDefaultIfBranchIsNotProvided()
 {
-  if [ -z "${BRANCH}" ] ; then
+  if [ -z "$BRANCH" ] ; then
     echo "${info}BRANCH is undefined so checking out dev${normal}"
     BRANCH="dev"
   fi
@@ -144,24 +144,24 @@ setDefaultIfBranchIsNotProvided()
 
 setWorkingDirectoryIfProvided()
 {
-  if [ -z "${WORKING_DIR}" ] ; then
+  if [ -z "$WORKING_DIR" ] ; then
     echo "${info}WORKING_DIR is undefined so setting to $PWD${normal}"
     WORKING_DIR=$PWD
   else
-    echo "${info}Working dir is $WORKING_DIR${normal}"
+    echo "${info}Working directory is ${WORKING_DIR}${normal}"
   fi
 }
 
 setTargetDirectoryIfProvided()
 {
   echo $info
-  if [ -z "${TARGET_DIR}" ] ; then
-    echo "${info}TARGET_DIR is undefined so setting to $PWD"
+  if [ -z "$TARGET_DIR" ] ; then
+    echo "${info}TARGET_DIR is undefined so setting to ${PWD}"
     TARGET_DIR=$PWD
     # Only makes a difference if we're in Docker
     echo "If you're using Docker The build artifact will not be copied to the host"
   else
-    echo "${info}Target directory is $TARGET_DIR${normal}"
+    echo "${info}Target directory is ${TARGET_DIR}${normal}"
     COPY_TO_HOST=true
     echo "If you're using Docker we'll copy the build artifact to the host"
   fi
@@ -172,18 +172,18 @@ cloneOpenJDKGitRepo()
   echo $git
   if [ -d "${WORKING_DIR}/${OPENJDK_REPO_NAME}/.git" ] && [ "$REPOSITORY" == "AdoptOpenJDK/openjdk-jdk8u" ] ; then
     # It does exist and it's a repo other than the AdoptOpenJDK one
-    cd $WORKING_DIR/$OPENJDK_REPO_NAME
-    echo "${info}Will reset the repository at $PWD in 10 seconds...${git}"
+    cd ${WORKING_DIR}/${OPENJDK_REPO_NAME}
+    echo "${info}Will reset the repository at ${PWD} in 10 seconds...${git}"
     sleep 10
     echo "${git}Pulling latest changes from git repo"
     git fetch --all
-    git reset --hard origin/$BRANCH
-    echo $normal
+    git reset --hard origin/${BRANCH}
+    echo ${normal}
     cd $WORKING_DIR
   elif [ ! -d "${WORKING_DIR}/${OPENJDK_REPO_NAME}/.git" ] ; then
     # If it doesn't exixt, clone it
     echo "${info}Didn't find any existing openjdk repository at WORKING_DIR (set to ${WORKING_DIR}) so cloning the source to openjdk"
-    if [[ "${USE_SSH}" == "true" ]] ; then
+    if [[ "$USE_SSH" == "true" ]] ; then
       echo "git clone -b ${BRANCH} git@github.com:${REPOSITORY}.git ${WORKING_DIR}/${OPENJDK_REPO_NAME}"
       git clone -b ${BRANCH} git@github.com:${REPOSITORY}.git $WORKING_DIR/$OPENJDK_REPO_NAME
     else
@@ -196,8 +196,8 @@ cloneOpenJDKGitRepo()
 
 testOpenJDKViaDocker()
 {
-  if [[ ! -z $JTREG ]]; then
-    docker run --privileged -t -v $WORKING_DIR/$OPENJDK_REPO_NAME:/openjdk/jdk8u/openjdk --entrypoint jtreg.sh $CONTAINER
+  if [[ ! -z "$JTREG" ]]; then
+    docker run --privileged -t -v ${WORKING_DIR}/${OPENJDK_REPO_NAME}:/openjdk/jdk8u/openjdk --entrypoint jtreg.sh ${CONTAINER}
   fi
 }
 
@@ -205,7 +205,7 @@ buildAndTestOpenJDKViaDocker()
 {
   PS_DOCKER=$(ps -ef | grep "docker" | wc -l)
 
-  if [ -z $(which docker) ] || [ ${PS_DOCKER} -lt 2 ]; then
+  if [ -z $(which docker) ] || [ "$PS_DOCKER" -lt 2 ]; then
     echo "${error}Error, please install docker and ensure that it is in your path and running!${normal}"
     exit
   fi
@@ -232,28 +232,28 @@ buildAndTestOpenJDKViaDocker()
      echo $good
      docker ps -a | awk '{ print $1,$2 }' | grep $CONTAINER | awk '{print $1 }' | xargs -I {} docker rm -f {}
      docker build -t $CONTAINER docker/jdk8u/x86_64/ubuntu
-     echo $normal
+     echo ${normal}
   fi
 
-  docker run --privileged -t -v $WORKING_DIR/$OPENJDK_REPO_NAME:/openjdk/jdk8u/openjdk --entrypoint build.sh $CONTAINER
+  docker run --privileged -t -v ${WORKING_DIR}/${OPENJDK_REPO_NAME}:/openjdk/jdk8u/openjdk --entrypoint build.sh $CONTAINER
 
   testOpenJDKViaDocker
 
   CONTAINER_ID=$(docker ps -a | awk '{ print $1,$2 }' | grep openjdk_container | awk '{print $1 }'| head -1)
 
-  if [[ "${COPY_TO_HOST}" == "true" ]] ; then
-    echo "Copying to the host with docker cp $id:/openjdk/jdk8u/OpenJDK.tar.gz $TARGET_DIR"
-    docker cp $CONTAINER_ID:/openjdk/jdk8u/OpenJDK.tar.gz $TARGET_DIR
+  if [[ "$COPY_TO_HOST" == "true" ]] ; then
+    echo "Copying to the host with docker cp ${id}:/openjdk/jdk8u/OpenJDK.tar.gz ${TARGET_DIR}"
+    docker cp ${CONTAINER_ID}:/openjdk/jdk8u/OpenJDK.tar.gz $TARGET_DIR
   fi
 
-  if [[ "${JTREG}" == "true" ]] ; then
+  if [[ "$JTREG" == "true" ]] ; then
     echo "Copying jtreg reports from docker"
-    docker cp $CONTAINER_ID:/openjdk/jdk8u/jtreport.zip $TARGET_DIR
-    docker cp $CONTAINER_ID:/openjdk/jdk8u/jtwork.zip $TARGET_DIR
+    docker cp ${CONTAINER_ID}:/openjdk/jdk8u/jtreport.zip $TARGET_DIR
+    docker cp ${CONTAINER_ID}:/openjdk/jdk8u/jtwork.zip $TARGET_DIR
   fi
 
   # Didn't specify to keep
-  if [[ -z ${KEEP} ]] ; then
+  if [[ -z "$KEEP" ]] ; then
     docker ps -a | awk '{ print $1,$2 }' | grep $CONTAINER | awk '{print $1 }' | xargs -I {} docker rm {}
   fi
 }
@@ -268,7 +268,7 @@ testOpenJDKInNativeEnvironmentIfExpected()
 
 buildAndTestOpenJDKInNativeEnvironment()
 {
-  echo "Calling sbin/build.sh $WORKING_DIR $TARGET_DIR $OPENJDK_REPO_NAME $BUILD_FULL_NAME $JVM_VARIANT"
+  echo "Calling sbin/build.sh ${WORKING_DIR} ${TARGET_DIR} ${OPENJDK_REPO_NAME} ${BUILD_FULL_NAME} ${JVM_VARIANT}"
   $WORKING_DIR/sbin/build.sh $WORKING_DIR $TARGET_DIR $OPENJDK_REPO_NAME $BUILD_FULL_NAME $JVM_VARIANT
 
   testOpenJDKInNativeEnvironmentIfExpected
@@ -276,7 +276,7 @@ buildAndTestOpenJDKInNativeEnvironment()
 
 buildAndTestOpenJDK()
 {
-  if [ "${USE_DOCKER}" == "true" ] ; then
+  if [ "$USE_DOCKER" == "true" ] ; then
     buildAndTestOpenJDKViaDocker
   else
     buildAndTestOpenJDKInNativeEnvironment
@@ -287,7 +287,7 @@ buildAndTestOpenJDK()
 
 initialiseEscapeCodes
 sourceSignalHandler
-parseCommandLineArgs $@
+parseCommandLineArgs "$@"
 checkIfDockerIsUsedForBuildingOrNot
 checkInCaseOfDockerShouldTheContainerBePreserved
 setDefaultIfBranchIsNotProvided
