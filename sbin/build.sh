f39e89d409b690a69e0d757b68d02b7c1eae0b12
neomatrix369 <sadhak001@gmail.com>
2017-04-19 10:46:52 +0100

    Added a more reliable wayto get current directory and refer to the previous directory where the source file is.

--- sbin/build.sh
+++ sbin/build.sh
@@ -38,9 +38,12 @@ OS_MACHINE_NAME=$(uname -m)
 
 sourceFileWithColourCodes()
 {
+  CURRENT_SCRIPT=$(realpath "$0")
+  CURRENT_SCRIPTPATH=$(dirname "$CURRENT_SCRIPT")
+
   # shellcheck disable=SC1090
   # shellcheck disable=SC1091
-  source "$WORKING_DIR"/colour-codes.sh
+  source "$CURRENT_SCRIPTPATH"/../colour-codes.sh
 }
 
 checkIfDockerIsUsedForBuildingOrNot()
