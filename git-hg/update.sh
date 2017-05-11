8cebc9196a83173f8b82ee5bb5d2191ae1e0aca5
Rochmad Adhi Yunianto <rochmadadhi@gmail.com>
2017-05-11 23:58:22 +0700

    Add shellcheck disable without editing old script

--- git-hg/update.sh
+++ git-hg/update.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
+#shellcheck disable=SC1091
+#shellcheck disable=SC2086
+#shellcheck disable=SC2154
+#shellcheck disable=SC2006
+#shellcheck disable=SC2035
+#shellcheck disable=SC2164
 echo "Common defs"
 
-# shellcheck disable=SC1091
 . import-common.sh
 
 echo "Enter hg"
 
 cd hg || exit 1
-# shellcheck disable=SC2035
-# shellcheck disable=SC2006
+
 bpaths=`ls -d -1 */*`
 
 for bpath in $bpaths
@@ -20,7 +23,7 @@ echo "Update $bpath -> (root)"
 git hg fetch "http://hg.openjdk.java.net/$bpath"
 git hg pull "http://hg.openjdk.java.net/$bpath"
 popd
-# shellcheck disable=SC2154
+
 for m in $submodules
 do
     pushd "$bpath/$m"
@@ -40,21 +43,19 @@ echo "Check out $bpath"
 git checkout master || exit 1
 
 echo "Fetch (root)"
-# shellcheck disable=SC2086
+
 git fetch imports/$bpath/root || exit 1
 
 echo "Merge (root)"
-# shellcheck disable=SC2086
+
 git merge imports/$bpath/root/master -m "Merge from (root)" --no-ff || exit 1
 
 for m in $submodules
 do
     echo "Fetch '$m'"
-    # shellcheck disable=SC2086
     git fetch imports/$bpath/$m || exit 1
 
     echo "Merge '$m'"
-    # shellcheck disable=SC2086
     git subtree merge --prefix=$m imports/$bpath/$m/master -m "Merge from '$m'" || exit 1
 done
 
@@ -62,6 +63,6 @@ echo "Push"
 
 git push github master --tags
 
-cd ../hg || exit 1
+cd ../hg
 
 done
