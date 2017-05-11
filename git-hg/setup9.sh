16602f649421be2a939ddc5e8692cdf0eb0a5928
Rochmad Adhi Yunianto <rochmadadhi@gmail.com>
2017-05-11 23:52:06 +0700

    Add shellcheck disable without editing old script

--- git-hg/setup9.sh
+++ git-hg/setup9.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-rm -rf "$WORKSPACE"/combined "$WORKSPACE"/hg
-mkdir "$WORKSPACE"/combined
-mkdir "$WORKSPACE"/hg
-cd "$WORKSPACE"/combined || exit
+#shellcheck disable=SC2086
+#shellcheck disable=SC2164
+#shellcheck disable=SC2103
+rm -rf $WORKSPACE/combined $WORKSPACE/hg
+mkdir $WORKSPACE/combined
+mkdir $WORKSPACE/hg
+cd $WORKSPACE/combined
 git init
 git checkout -b root-commit
 git remote add github git@github.com:AdoptOpenJDK/openjdk-jdk9.git
-cd - || exit
+cd -
 bash add-branch.sh jdk9/jdk9
