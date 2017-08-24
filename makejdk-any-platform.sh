46f9687933e90d5d9eb26c84e85e4e8efc92c30b
Enrique Lacal Bereslawski <Enrique.Lacal.Bereslawski@ibm.com>
2017-08-24 13:11:24 +0100

    Switch to Case Statements and added case for openjdk versions

--- makejdk-any-platform.sh
+++ makejdk-any-platform.sh
@@ -50,30 +50,54 @@ fi
 export REPOSITORY="${REPOSITORY:-adoptopenjdk/openjdk-$OPENJDK_VERSION}";
 export REPOSITORY="$(echo "${REPOSITORY}" | awk '{print tolower($0)}')";
 
-if [[ "$OS_MACHINE_NAME" == "s390x" ]] ; then
-   export JVM_VARIANT=${JVM_VARIANT:-zero}
-   export BUILD_FULL_NAME=${BUILD_FULL_NAME:-linux-s390x-normal-${JVM_VARIANT}-release}
-   S390X_MAKE_ARGS="CONF=${BUILD_FULL_NAME} DEBUG_BINARIES=true images"
-   export MAKE_ARGS_FOR_ANY_PLATFORM=${MAKE_ARGS_FOR_ANY_PLATFORM:-$S390X_MAKE_ARGS}
-fi
-if [[ "$OS_MACHINE_NAME" = "ppc64le" ]] ; then
+case "$OS_MACHINE_NAME" in
+"s390x")
+  case "OPENJDK_VERSION" in
+     "jdk9")
+     export JVM_VARIANT=${JVM_VARIANT:-server}
+     export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--disable-warnings-as-errors"};;
+     "jdk8u")
+     export JVM_VARIANT=${JVM_VARIANT:-zero} ;;
+  esac
+
+  export BUILD_FULL_NAME=${BUILD_FULL_NAME:-linux-s390x-normal-${JVM_VARIANT}-release}
+  S390X_MAKE_ARGS="CONF=${BUILD_FULL_NAME} DEBUG_BINARIES=true images"
+  export MAKE_ARGS_FOR_ANY_PLATFORM=${MAKE_ARGS_FOR_ANY_PLATFORM:-$S390X_MAKE_ARGS}
+;;
+
+"ppc64le")
+  case "OPENJDK_VERSION" in
+    "jdk9")
+    export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--disable-warnings-as-errors"};;
+  esac
   export JVM_VARIANT=${JVM_VARIANT:-server}
   export BUILD_FULL_NAME=${BUILD_FULL_NAME:-linux-ppc64-normal-${JVM_VARIANT}-release}
   # shellcheck disable=SC1083
   export FREETYPE_FONT_BUILD_TYPE_PARAM=${FREETYPE_FONT_BUILD_TYPE_PARAM:="--build=$(rpm --eval %{_host})"}
-fi
-if [[ "$OS_MACHINE_NAME" == "armv7l" ]] ; then
-   export JVM_VARIANT=${JVM_VARIANT:-zero}
-   export MAKE_ARGS_FOR_ANY_PLATFORM=${MAKE_ARGS_FOR_ANY_PLATFORM:-"DEBUG_BINARIES=true images"}
-   export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--with-jobs=${NUM_PROCESSORS}"}
-fi
+;;
 
-if [[ "$OS_KERNEL_NAME" == "aix" ]] ; then
-   export MAKE_COMMAND_NAME=${MAKE_COMMAND_NAME:-"gmake"}
-fi
+"armv7l")
+  export JVM_VARIANT=${JVM_VARIANT:-zero}
+  export MAKE_ARGS_FOR_ANY_PLATFORM=${MAKE_ARGS_FOR_ANY_PLATFORM:-"DEBUG_BINARIES=true images"}
+  export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--with-jobs=${NUM_PROCESSORS}"}
+;;
 
-if [[ "$OS_MACHINE_NAME" == "aarch64" ]] ; then
-   export FREETYPE_FONT_VERSION="2.5.2"
-fi
+"aix")
+  case "OPENJDK_VERSION" in
+    "jdk9")
+    export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--disable-warnings-as-errors"};;
+  esac
+  export MAKE_COMMAND_NAME=${MAKE_COMMAND_NAME:-"gmake"}
+;;
+
+"aarch64")
+  case "OPENJDK_VERSION" in
+    "jdk9")
+    export CONFIGURE_ARGS_FOR_ANY_PLATFORM=${CONFIGURE_ARGS_FOR_ANY_PLATFORM:-"--disable-warnings-as-errors"};;
+  esac
+  export FREETYPE_FONT_VERSION="2.5.2"
+;;
+
+esac
 
 ./makejdk.sh "$@"
