c7135afd673b66733bc19f94f247c9292998424d
Enrique Lacal Bereslawski <Enrique.Lacal.Bereslawski@ibm.com>
2017-08-25 17:21:28 +0100

    Remove quotes

--- sbin/build.sh
+++ sbin/build.sh
@@ -222,10 +222,8 @@ buildOpenJDK()
 
 printJavaVersionString()
 {
-
-  echo "PATH_ JDK : ${JDK_PATH}"
-  echo "$OPENJDK_DIR"
-  PRODUCT_HOME=$(ls -d "$OPENJDK_DIR/build/*/images/${JDK_PATH}")
+  # shellcheck disable=SC2046
+  PRODUCT_HOME=$(ls -d $OPENJDK_DIR/build/*/images/${JDK_PATH})
   if [[ -d "$PRODUCT_HOME" ]]; then
      echo "${good}'$PRODUCT_HOME' found${normal}"
      # shellcheck disable=SC2154
@@ -253,7 +251,7 @@ removingUnnecessaryFiles()
 
   echo "moving ${JDK_PATH} to ${OPENJDK_REPO_TAG}"
   rm -rf "${OPENJDK_REPO_TAG}" || true
-  mv "$JDK_PATH" "${OPENJDK_REPO_TAG}"
+  mv $JDK_PATH "${OPENJDK_REPO_TAG}"
 
   # Remove files we don't need
   rm -rf "${OPENJDK_REPO_TAG}"/demo/applets || true
