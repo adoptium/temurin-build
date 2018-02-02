861a38210689943c5c03f046a993b54126d9a29f
neomatrix369 <sadhak001@gmail.com>
2018-02-02 00:17:21 +0000

    Fixed usage of $@, post shellcheck warning

--- mercurial-tags/java-tool/runTool.sh
+++ mercurial-tags/java-tool/runTool.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "*********** Running the mercurial tracker tool on the respective repo with parameters: '$@'..."
+ALL_ARGUMENTS="$@"
+echo "*********** Running the mercurial tracker tool on the respective repo with parameters: '${ALL_ARGUMENTS}'..."
 cd "${SCRIPT_DIR}" || true
-java MercurialTracker "$@"
+java MercurialTracker "${ALL_ARGUMENTS}"
