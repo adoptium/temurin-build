18a87b388502aa03003b8e2b2a6ae8b73193d03a
neomatrix369 <sadhak001@gmail.com>
2018-02-02 00:00:02 +0000

    Displaying arguments passed into runTool script, on the logs

--- mercurial-tags/java-tool/runTool.sh
+++ mercurial-tags/java-tool/runTool.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "*********** Running the mercurial tracker tool..."
+echo "*********** Running the mercurial tracker tool on the respective repo with parameters: '$@'..."
 cd "${SCRIPT_DIR}" || true
 java MercurialTracker "$@"
