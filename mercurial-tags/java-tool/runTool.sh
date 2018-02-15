e75711a5622b8195f61b56391634a97c0e84f061
neomatrix369 <sadhak001@gmail.com>
2018-02-15 18:51:03 +0000

    Added set -x flag around java Mercu.. command to see what parameters are getting passed thru

--- mercurial-tags/java-tool/runTool.sh
+++ mercurial-tags/java-tool/runTool.sh
@@ -5,4 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ALL_ARGUMENTS="$*"
 echo "*********** Running the mercurial tracker tool on the respective repo with parameters: '${ALL_ARGUMENTS}'..."
 cd "${SCRIPT_DIR}" || true
+
+set -x
 java MercurialTracker "${ALL_ARGUMENTS}"
+set +x
