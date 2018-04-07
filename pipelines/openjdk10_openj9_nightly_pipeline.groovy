f2204f3a0ca39d833838258b37ac226a166c322d
Stewart X Addison <sxa@uk.ibm.com>
2018-04-07 13:41:29 +0100

    Add Windows to openjdk10_openj9 pipeline

--- pipelines/openjdk10_openj9_nightly_pipeline.groovy
+++ pipelines/openjdk10_openj9_nightly_pipeline.groovy
@@ -1,11 +1,12 @@
 println "building ${JDK_VERSION}"
 
-def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX']
+def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX', "Windows"]
 def buildMaps = [:]
 buildMaps['Linux'] = [test:false, ArchOSs:'x86-64_linux']
 buildMaps['zLinux'] = [test:false, ArchOSs:'s390x_linux']
 buildMaps['ppc64le'] = [test:false, ArchOSs:'ppc64le_linux']
 buildMaps['AIX'] = [test:false, ArchOSs:'ppc64_aix']
+buildMaps['Windows'] = [test:false, ArchOSs:'x86-64_windows']
 def typeTests = ['openjdktest', 'systemtest']
 
 def jobs = [:]
