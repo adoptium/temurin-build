f7d95bbcfcd04bc7d671d73f3eacdebe3326036f
John Oliver <johno@insightfullogic.com>
2018-05-04 14:43:46 +0100

    fix windows

--- build-farm/make-adopt-build-farm-jdk8u.sh
+++ build-farm/make-adopt-build-farm-jdk8u.sh
@@ -11,6 +11,8 @@ TIMESTAMP="$(date +'%Y%d%m%H%M')"
 OPTIONS=""
 PLATFORM=""
 EXTENSION=""
+CONFIGURE_ARGS_FOR_ANY_PLATFORM=""
+
 export JDK_BOOT_DIR=$JDK7_BOOT_DIR
 
 if [[ $NODE_LABELS = *"linux"* ]] ; then
@@ -35,11 +37,11 @@ elif [[ $NODE_LABELS = *"windows"* ]] ; then
   export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/:/cygdrive/C/Projects/OpenJDK/make-3.82/:$PATH"
   export LANG=C
   export JAVA_HOME=$JDK7_BOOT_DIR
-  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="with_freetype=/cygdrive/C/Projects/OpenJDK/freetype --disable-ccache"
+  CONFIGURE_ARGS_FOR_ANY_PLATFORM="with_freetype=/cygdrive/C/Projects/OpenJDK/freetype --disable-ccache"
 fi
 
 # Set the file name
 FILENAME="OpenJDK8_x64_${PLATFORM}_${TIMESTAMP}.${EXTENSION}"
 echo "Filename will be: $FILENAME"
 
-bash "$SCRIPT_DIR/../makejdk-any-platform.sh" --jdk-boot-dir "${JDK_BOOT_DIR}" --target-file-name "${FILENAME}" "${GIT_SHALLOW_CLONE_OPTION}" "${TAG_OPTION}" "${OPTIONS}" jdk8u
+bash "$SCRIPT_DIR/../makejdk-any-platform.sh" --jdk-boot-dir "${JDK_BOOT_DIR}" --configure-args "${CONFIGURE_ARGS_FOR_ANY_PLATFORM}" --target-file-name "${FILENAME}" "${GIT_SHALLOW_CLONE_OPTION}" "${TAG_OPTION}" "${OPTIONS}" jdk8u
