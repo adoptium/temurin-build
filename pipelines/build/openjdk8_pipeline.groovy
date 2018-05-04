4f98501e707d87337665c3a0a0c6bdf4a695bc00
John Oliver <johno@insightfullogic.com>
2018-05-04 16:28:44 +0100

    test shared config

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -11,41 +11,6 @@ if (osTarget != "all") {
             .findAll { it.key == osTarget }
 }
 
-def buildJobs = []
-def jobs = [:]
+def OpenJDKBuild = load("OpenJDKBuild.groovy")
 
-buildConfigurations.each { buildConfiguration ->
-    def configuration = buildConfiguration.value
-
-    def buildType = "${configuration.os}-${configuration.arch}"
-
-    jobs[buildType] = {
-        stage("build-${buildType}") {
-            def buildJob = build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${configuration.aditionalNodeLabels}&&${configuration.os}&&${configuration.arch}"]]
-            buildJobs.add([
-                    job        : buildJob,
-                    config     : configuration,
-                    targetLabel: buildConfiguration.key
-            ]);
-        }
-
-    }
-}
-parallel jobs
-
-node('centos6&&x64&&build') {
-    buildJobs.each {
-        buildJob ->
-            if (buildJob.job.getResult() == 'SUCCESS') {
-                copyArtifacts(
-                        projectName: 'openjdk8_build-refactor',
-                        selector: specific("${buildJob.job.getNumber()}"),
-                        filter: 'workspace/target/*',
-                        fingerprintArtifacts: true,
-                        target: "target/${buildJob.targetLabel}/${buildJob.config.arch}/",
-                        flatten: true)
-            }
-    }
-
-    archiveArtifacts artifacts: 'target/*/*/*'
-}
+OpenJDKBuild.build(buildConfigurations)
