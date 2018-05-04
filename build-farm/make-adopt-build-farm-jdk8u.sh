3a00a90e50c5d9b8a4aec4cfed4b6a0bc40d7b16
John Oliver <johno@insightfullogic.com>
2018-05-04 13:31:19 +0100

    Remove after testing

--- build-farm/make-adopt-build-farm-jdk8u.sh
+++ build-farm/make-adopt-build-farm-jdk8u.sh
@@ -42,6 +42,7 @@ FILENAME="OpenJDK8_x64_$PLATFORM_$TIMESTAMP.$EXTENSION"
 echo "Filename will be: $FILENAME"
 
 echo "foo">bar.txt
+mkdir -p "workspace/target/"
 tar -cvzf "workspace/target/test.$EXTENSION" bar.txt
 
 #bash "$SCRIPT_DIR/../makejdk-any-platform.sh" --jdk-boot-dir "$JDK_BOOT_DIR" --target-file-name "$FILENAME" $GIT_SHALLOW_CLONE_OPTION $TAG_OPTION $OPTIONS jdk8u
