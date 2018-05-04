4306ac5ecf874b694de59f89b6e1e3b18963c7e1
John Oliver <johno@insightfullogic.com>
2018-05-04 13:03:04 +0100

    Full build and archive

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -3,21 +3,21 @@ def buildConfigurations = [
         [os: 'centos6', arch: 'x64']
 ]
 
+def buildJobs = []
 def jobs = [:]
 for (int i = 0; i < buildConfigurations.size(); i++) {
     def index = i
     def config = buildConfigurations[index]
 
     def buildType = "${config.os}-${config.arch}"
-    def buildJobNum
 
     jobs[buildType] = {
         stage("build-${buildType}") {
-            //buildJob = build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
-            //buildJobNum = buildJob.getNumber()
-            //archiveArtifacts artifacts: 'workspace/target/*.tar.gz, workspace/target/*.zip'
-
-
+            buildJob = build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
+            buildJobs.add([
+                    job   : buildJob,
+                    config: config
+            ]);
         }
 
     }
@@ -25,12 +25,18 @@ for (int i = 0; i < buildConfigurations.size(); i++) {
 parallel jobs
 
 node {
-    copyArtifacts(
-            projectName: 'openjdk8_build-refactor',
-            selector: specific("13"),
-            filter: 'workspace/target/*',
-            fingerprintArtifacts: true,
-            target: 'target/${config.arch}/',
-            flatten: true)
+    buildJobs.each {
+        buildJob ->
+            if (buildJob.job.getResult() == 'SUCCESS') {
+                copyArtifacts(
+                        projectName: 'openjdk8_build-refactor',
+                        selector: specific(buildJob.getNumber()),
+                        filter: 'workspace/target/*',
+                        fingerprintArtifacts: true,
+                        target: "target/${buildJob.config.arch}/",
+                        flatten: true)
+            }
+    }
+
     archiveArtifacts artifacts: 'target/*/*'
 }
