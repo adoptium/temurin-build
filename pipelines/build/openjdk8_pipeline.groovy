ac1b5d69b16d531be85fc19af18d3829f4d0dd50
John Oliver <johno@insightfullogic.com>
2018-05-04 12:28:19 +0100

    Try to fix archive

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -13,8 +13,6 @@ for (int i = 0; i < buildConfigurations.size(); i++) {
     jobs[buildType] = {
         stage("build-${buildType}") {
             build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
-        }
-        stage("archive-${buildType}") {
             archiveArtifacts artifacts: 'workspace/target/*.tar.gz, workspace/target/*.zip'
         }
     }
