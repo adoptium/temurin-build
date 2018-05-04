780ac09c5010ded0ed2ae0876214c8e761adf4b8
John Oliver <johno@insightfullogic.com>
2018-05-04 12:48:16 +0100

    Try to fix archive

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -18,10 +18,17 @@ for (int i = 0; i < buildConfigurations.size(); i++) {
                 copyArtifacts(
                         projectName: 'openjdk8_build-refactor',
                         selector: specific("13"),
-                        filter: 'workspace/target/*.tar.gz,workspace/target/*.zip',
+                        filter: 'workspace/target/*.tar.gz',
                         fingerprintArtifacts: true,
                         target: 'target/${config.arch}/',
-                        flatten: true,)
+                        flatten: true)
+                copyArtifacts(
+                        projectName: 'openjdk8_build-refactor',
+                        selector: specific("13"),
+                        filter: 'workspace/target/*.zip',
+                        fingerprintArtifacts: true,
+                        target: 'target/${config.arch}/',
+                        flatten: true)
             }
             //buildJob = build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
             //buildJobNum = buildJob.getNumber()
