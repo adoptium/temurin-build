6fc1366049c0874c5736085f2747a64bb492aea6
John Oliver <johno@insightfullogic.com>
2018-05-04 14:09:26 +0100

    debug

--- build-farm/make-adopt-build-farm-jdk8u.sh
+++ build-farm/make-adopt-build-farm-jdk8u.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eux
+set -ex
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -15,7 +15,7 @@ if [[ $NODE_LABELS = *"linux"* ]] ; then
   EXTENSION=tar.gz
 
   export JDK_BOOT_DIR=$JDK7_BOOT_DIR
-  if [ ! -z "$TAG" ]; then
+  if [ ! -z "${TAG}" ]; then
     OPTIONS="${OPTIONS} --tag $TAG"
   fi
   #OPTIONS="${OPTIONS} --tmp-space-build"
