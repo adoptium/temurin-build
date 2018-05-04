a4c0e1adc8da39ef4240d6345c730414583e733b
John Oliver <johno@insightfullogic.com>
2018-05-04 12:42:45 +0100

    Try to fix archive

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -9,12 +9,26 @@ for (int i = 0; i < buildConfigurations.size(); i++) {
     def config = buildConfigurations[index]
 
     def buildType = "${config.os}-${config.arch}"
+    def buildJobNum
 
     jobs[buildType] = {
         stage("build-${buildType}") {
-            build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
-            archiveArtifacts artifacts: 'workspace/target/*.tar.gz, workspace/target/*.zip'
+
+
+            copyArtifacts(
+                    projectName: 'openjdk8_build-refactor',
+                    selector: specific("13"),
+                    filter: 'workspace/target/*.tar.gz',
+                    fingerprintArtifacts: true,
+                    target: 'target/${config.arch}/',
+                    flatten: true,);
+            //buildJob = build job: "openjdk8_build-refactor", parameters: [[$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${config.os}&&${config.arch}&&build"]]
+            //buildJobNum = buildJob.getNumber()
+            //archiveArtifacts artifacts: 'workspace/target/*.tar.gz, workspace/target/*.zip'
+
+
         }
+
     }
 }
 parallel jobs
