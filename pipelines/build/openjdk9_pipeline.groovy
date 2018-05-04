2611567e9cb0b9b303b6a017d499e7b7a9ffe708
John Oliver <johno@insightfullogic.com>
2018-05-04 17:07:33 +0100

    test shared config

--- pipelines/build/openjdk9_pipeline.groovy
+++ pipelines/build/openjdk9_pipeline.groovy
@@ -10,11 +10,61 @@ if (osTarget != "all") {
     buildConfigurations = buildConfigurations
             .findAll { it.key == osTarget }
 }
-// Todo actually do this in a shared lib to avoid tying up a node
-node {
-    checkout scm
-    def rootDir = pwd()
-    def OpenJDKBuild = load("${rootDir}/pipelines/build/OpenJDKBuild.groovy")
 
-    OpenJDKBuild.doBuild("jdk9", buildConfigurations)
+doBuild("jdk9", buildConfigurations)
+
+
+
+
+///////////////////////////////////////////////////
+//Do build is the same for all pipelines
+//TODO: make it a shared library
+def doBuild(javaToBuild, buildConfigurations) {
+    if (osTarget != "all") {
+        buildConfigurations = buildConfigurations
+                .findAll { it.key == osTarget }
+    }
+
+    def buildJobs = []
+    def jobs = [:]
+
+    buildConfigurations.each { buildConfiguration ->
+        def configuration = buildConfiguration.value
+
+        def buildType = "${configuration.os}-${configuration.arch}"
+
+        jobs[buildType] = {
+            stage("build-${buildType}") {
+                def buildJob = build job: "openjdk_build-refactor", parameters: [
+                        string(name: 'JAVA_TO_BUILD', value: "${javaToBuild}"),
+                        [$class: 'LabelParameterValue', name: 'NODE_LABEL', label: "${configuration.aditionalNodeLabels}&&${configuration.os}&&${configuration.arch}"]
+                ]
+
+                buildJobs.add([
+                        job        : buildJob,
+                        config     : configuration,
+                        targetLabel: buildConfiguration.key
+                ]);
+            }
+
+        }
+    }
+    parallel jobs
+
+    node('centos6&&x64&&build') {
+        buildJobs.each {
+            buildJob ->
+                if (buildJob.job.getResult() == 'SUCCESS') {
+                    copyArtifacts(
+                            projectName: 'openjdk_build-refactor',
+                            selector: specific("${buildJob.job.getNumber()}"),
+                            filter: 'workspace/target/*',
+                            fingerprintArtifacts: true,
+                            target: "target/${buildJob.targetLabel}/${buildJob.config.arch}/",
+                            flatten: true)
+                }
+        }
+
+        archiveArtifacts artifacts: 'target/*/*/*'
+    }
 }
