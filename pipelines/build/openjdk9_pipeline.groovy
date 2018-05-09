7ebe6212c4c66279c632b2353c48cf14f0a59333
John Oliver <johno@insightfullogic.com>
2018-05-09 16:08:02 +0100

    Drop to jdk7 for win build

--- pipelines/build/openjdk9_pipeline.groovy
+++ pipelines/build/openjdk9_pipeline.groovy
@@ -20,9 +20,9 @@ def buildConfigurations = [
         windows: [
                 os                 : 'windows',
                 arch               : 'x64',
-                bootJDK            : "8",
+                bootJDK            : "7",
                 path               : "/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/",
-                configureArgs      : "with_freetype=/cygdrive/C/openjdk/freetype --disable-ccache",
+                configureArgs      : "with_freetype=/cygdrive/C/openjdk/freetype",
                 aditionalNodeLabels: 'build&&win2012'
         ]
 ]
