a61d88657d2e37e50c9ad6f995ff337b06f34968
John Oliver <johno@insightfullogic.com>
2018-05-10 12:26:40 +0100

    fix node

--- pipelines/build/openjdk9_pipeline.groovy
+++ pipelines/build/openjdk9_pipeline.groovy
@@ -23,7 +23,7 @@ def buildConfigurations = [
                 bootJDK            : "8",
                 path               : "/usr/bin:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/",
                 configureArgs      : "--with-freetype=/cygdrive/C/openjdk/freetype --disable-ccache --disable-javac-server",
-                aditionalNodeLabels: 'build&&win2012'
+                aditionalNodeLabels: 'buildj9&&win2012'
         ]
 ]
 
