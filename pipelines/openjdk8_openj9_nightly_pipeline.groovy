8b6d1772459fe20ca517a4fc31f8aebf4ab481f7
smlambert <slambert@gmail.com>
2018-06-04 16:26:33 -0400

    Add Win32 & LinuxLargeHeap
    Signed-off-by: smlambert <slambert@gmail.com>

--- pipelines/openjdk8_openj9_nightly_pipeline.groovy
+++ pipelines/openjdk8_openj9_nightly_pipeline.groovy
@@ -1,12 +1,14 @@
 println "building ${JDK_VERSION}"
 
-def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX', 'Windows']
+def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX', 'Windows', 'Windows32', 'LinuxXL']
 def buildMaps = [:]
 buildMaps['Linux'] = [test:['openjdktest', 'systemtest', 'perftest', 'externaltest'], ArchOSs:'x86-64_linux']
+buildMaps['LinuxXL'] = buildMaps['AIX'] = [test:false, ArchOSs:'x86-64_linux_largeHeap']
 buildMaps['zLinux'] = [test:['openjdktest', 'systemtest'], ArchOSs:'s390x_linux']
 buildMaps['ppc64le'] = [test:['openjdktest', 'systemtest'], ArchOSs:'ppc64le_linux']
 buildMaps['AIX'] = [test:false, ArchOSs:'ppc64_aix']
 buildMaps['Windows'] = [test:['openjdktest'], ArchOSs:'x86-64_windows']
+buildMaps['Windows32'] = [test:['openjdktest'], ArchOSs:'x86-32_windows']
 
 def jobs = [:]
 for ( int i = 0; i < buildPlatforms.size(); i++ ) {
