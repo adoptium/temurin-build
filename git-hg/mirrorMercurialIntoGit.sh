835900bdb0dbb4fa8e059c955c6a5b8366c16101
George Adams <george.adams@uk.ibm.com>
2018-06-06 11:57:01 +0100

    add force flag to git checkout for git-hg mirroring
    prevents untracked files from failing the job

--- git-hg/mirrorMercurialIntoGit.sh
+++ git-hg/mirrorMercurialIntoGit.sh
@@ -64,7 +64,7 @@ function addMercurialUpstream() {
   cd "$GITHUB_REPO" || exit 1
 
   git fetch origin
-  if ! git checkout "$BRANCH" ; then
+  if ! git checkout -f "$BRANCH" ; then
     git checkout -b "$BRANCH" || exit 1
   else
     git reset --hard origin/"$BRANCH" || echo "Not resetting as no upstream exists"
