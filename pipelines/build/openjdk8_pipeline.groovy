d0216b7a29d90242f2d03670e8f558e2468fb14b
John Oliver <johno@insightfullogic.com>
2018-06-06 15:42:59 +0100

    add aix for jdk8

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -27,7 +27,7 @@ def buildConfigurations = [
         aix    : [
                 os                 : 'aix',
                 arch               : 'ppc64',
-                bootJDK            : "8",
+                bootJDK            : "7",
                 path               : "/opt/freeware/bin:/usr/local/bin:/opt/IBM/xlC/13.1.3/bin:/opt/IBM/xlc/13.1.3/bin",
                 configureArgs      : "--disable-warnings-as-errors --with-memory-size=18000 --with-cups-include=/opt/freeware/include --with-extra-ldflags=-lpthread --with-extra-cflags=-lpthread --with-extra-cxxflags=-lpthread",
                 buildArgs          : '--skip-freetype',
