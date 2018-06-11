9c3789f4cf06b007001554bffc5d6d45a9a3d8a7
John Oliver <johno@insightfullogic.com>
2018-06-11 14:55:40 +0100

    Force cleanup

--- pipelines/build/openjdk8_pipeline.groovy
+++ pipelines/build/openjdk8_pipeline.groovy
@@ -106,6 +106,9 @@ def doBuild(javaToBuild, buildConfigurations, osTarget) {
         parallel jobs
     } finally {
         node('master') {
+            sh "rm -rf target || true"
+            sh "mkdir target || true"
+
             buildJobs.each {
                 buildJob ->
                     def job = buildJob.value
