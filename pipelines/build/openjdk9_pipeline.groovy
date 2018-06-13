4aff36fb54fe5aa693803b5f9113c52e832558f0
John Oliver <johno@insightfullogic.com>
2018-06-13 16:49:39 +0100

    Test using shared lib

--- pipelines/build/openjdk9_pipeline.groovy
+++ pipelines/build/openjdk9_pipeline.groovy
@@ -57,6 +57,6 @@ def javaToBuild = "jdk9"
 
 node ("master") {
     checkout scm
-    def buildFile = load "${WORKSPACE}/openjdk-build/pipelines/build/BuildBaseFile.groovy"
+    def buildFile = load "pipelines/build/BuildBaseFile.groovy"
     buildFile.doBuild(javaToBuild, buildConfigurations, osTarget)
 }
