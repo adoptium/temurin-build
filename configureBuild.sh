a7a6cbaf54ebf0932f007c8561cc3bcedcde8729
John Oliver <johno@insightfullogic.com>
2018-06-15 11:57:05 +0100

    Test strange behaviour

--- configureBuild.sh
+++ configureBuild.sh
@@ -133,13 +133,7 @@ setVariablesForConfigure() {
 
 # Set the repository to build from
 setRepository() {
-  local forest;
-  forest="${BUILD_CONFIG[OPENJDK_FOREST_NAME]}"
-  if [ "$forest" == "jdk9" ]
-  then
-    forest="jdk9u"
-  fi
-  local repository="${BUILD_CONFIG[REPOSITORY]:-adoptopenjdk/openjdk-${forest}";
+  local repository="${BUILD_CONFIG[REPOSITORY]:-adoptopenjdk/openjdk-${BUILD_CONFIG[OPENJDK_FOREST_NAME]}}";
   repository="$(echo "${repository}" | awk '{print tolower($0)}')";
 
   BUILD_CONFIG[REPOSITORY]=$repository
