945d689adeb4e44bf77164410a5774309a34835a
John Oliver <johno@insightfullogic.com>
2018-06-15 11:59:33 +0100

    Very quick hack to test jdk9u

--- configureBuild.sh
+++ configureBuild.sh
@@ -133,10 +133,15 @@ setVariablesForConfigure() {
 
 # Set the repository to build from
 setRepository() {
-  local repository="${BUILD_CONFIG[REPOSITORY]:-adoptopenjdk/openjdk-${BUILD_CONFIG[OPENJDK_FOREST_NAME]}}";
+  local forest;
+  forest="${BUILD_CONFIG[OPENJDK_FOREST_NAME]}";
+  if [ "$forest" == "jdk9" ]; then
+    forest="jdk9u";
+  fi
+  local repository="${BUILD_CONFIG[REPOSITORY]:-adoptopenjdk/openjdk-${forest}";
   repository="$(echo "${repository}" | awk '{print tolower($0)}')";
 
-  BUILD_CONFIG[REPOSITORY]=$repository
+  BUILD_CONFIG[REPOSITORY]=$repository;
 }
 
 # Specific platforms need to have special build settings
