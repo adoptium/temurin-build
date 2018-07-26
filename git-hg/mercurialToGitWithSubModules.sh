eaa6fdf17e66fef2136ce2a686a45ed5dcc4cf75
John Oliver <johno@insightfullogic.com>
2018-07-26 13:33:04 +0100

    Remove debug

--- git-hg/mercurialToGitWithSubModules.sh
+++ git-hg/mercurialToGitWithSubModules.sh
@@ -65,8 +65,6 @@ MIRROR=$WORKSPACE/openjdk-clean-mirror
 REWRITE_WORKSPACE=$WORKSPACE/openjdk-rewritten-mirror/
 REPO_LOCATION=$WORKSPACE/adoptopenjdk-clone/
 
-export DEBUG_SCRIPT="true"
-
 if [ -z ${DEBUG_SCRIPT+x} ]; then
   rm -rf "$REWRITE_WORKSPACE"
   mkdir -p "$REWRITE_WORKSPACE"
