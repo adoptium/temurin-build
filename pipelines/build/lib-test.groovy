e8fd5bba26fa92495f574efd910e906440e36dbc
John Oliver <johno@insightfullogic.com>
2018-09-17 14:03:39 +0100

    Lib test

--- pipelines/build/lib-test.groovy
+++ pipelines/build/lib-test.groovy
@@ -6,3 +6,5 @@ println JobHelper
 println NodeHelper
 
 println JobHelper.jobIsRunnable("build-scripts/old-stuff/create-build-job")
+println NodeHelper.nodeIsOnline("linux")
+println NodeHelper.nodeIsOnline("ubuntu16&&openj9")
