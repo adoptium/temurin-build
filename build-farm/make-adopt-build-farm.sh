db7c857a276ffe4a7019fc1b08455b73e020759d
John Oliver <johno@insightfullogic.com>
2018-09-18 12:24:27 +0100

    Fix head jdk build (#524)
    * Fix import method
    
    * Lib test
    
    * Lib test
    
    * Fix lib import yet again
    
    * Try to fix cacerts
    
    * Fix mac tar not supporting wildcards
    
    * Match specifically
    
    * Remove wildcard
    
    * Reduce download by 40mb by not being an idiot
    
    * Fix jdk build
    
    * Fix jdk build

--- build-farm/make-adopt-build-farm.sh
+++ build-farm/make-adopt-build-farm.sh
@@ -56,8 +56,7 @@ case "${JDK_BOOT_VERSION}" in
       "9")    export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK9_BOOT_DIR}";;
       "10")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK10_BOOT_DIR}";;
       "11")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK11_BOOT_DIR}";;
-      "home") export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JAVA_HOME}";;
-      *)    export JDK_BOOT_DIR="${JDK_BOOT_VERSION}";;
+      *)    export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK11_BOOT_DIR}";;
 esac
 
 echo "Boot jdk: ${JDK_BOOT_DIR}"
