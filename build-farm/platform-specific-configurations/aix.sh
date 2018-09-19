2816e42e810ef812ba3d8cea2c0914d8b3f51fa0
John Oliver <johno@insightfullogic.com>
2018-09-19 12:02:26 +0100

    Remove pthread flags from java 11

--- build-farm/platform-specific-configurations/aix.sh
+++ build-farm/platform-specific-configurations/aix.sh
@@ -20,7 +20,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/../../sbin/common/constants.sh"
 
 export PATH="/opt/freeware/bin:/usr/local/bin:/opt/IBM/xlC/13.1.3/bin:/opt/IBM/xlc/13.1.3/bin:$PATH"
-export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-memory-size=18000 --with-cups-include=/opt/freeware/include --with-extra-ldflags=-lpthread --with-extra-cflags=-lpthread --with-extra-cxxflags=-lpthread"
+export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-memory-size=18000 --with-cups-include=/opt/freeware/include"
+
+if [ "${JAVA_TO_BUILD}" != "${JDK11_VERSION}" ]
+then
+  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-ldflags=-lpthread --with-extra-cflags=-lpthread --with-extra-cxxflags=-lpthread"
+fi
+
 export BUILD_ARGS="${BUILD_ARGS} --skip-freetype"
 
 if [ "${ARCHITECTURE}" == "x64" ] && [ "${VARIANT}" == "openj9" ];
