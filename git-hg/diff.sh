8ece1d5539f887c17ec003a0b38c48972b9bdc02
Martijn Verburg <martijnverburg@gmail.com>
2018-09-25 18:22:08 +0100

    HTTPS everywhere

--- git-hg/diff.sh
+++ git-hg/diff.sh
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -52,7 +52,7 @@ function cloneRepos() {
   echo "OpenJDK Mercurial Repo Version: ${hg_root_forest}/${hg_repo_version}"
 
   git clone -b master "https://github.com/AdoptOpenJDK/openjdk-${git_repo_version}.git" openjdk-git || exit 1
-  hg clone "http://hg.openjdk.java.net/${hg_root_forest}/${hg_repo_version}" openjdk-hg || exit 1
+  hg clone "https://hg.openjdk.java.net/${hg_root_forest}/${hg_repo_version}" openjdk-hg || exit 1
 }
 
 function updateMercurialClone() {
--- git-hg/mercurialToGit.sh
+++ git-hg/mercurialToGit.sh
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,7 +38,7 @@ echo "You are in $WORKSPACE"
 function checkArgs() {
   if [ "$1" -lt 1 ]; then
      echo Usage: "$0" '[jdk-updates/jdk10u|jdk/jdk] (branch)'
-     echo "Hg Repo supplied should match a repository in http://hg.openjdk.java.net/"
+     echo "Hg Repo supplied should match a repository in https://hg.openjdk.java.net/"
      echo "For example, to get the latest jdk development repo:"
      echo "$0 jdk/jdk"
      echo "For example, to get the raw-string-literals branch from the amber repo:"
@@ -72,8 +72,8 @@ function addMercurialUpstream() {
 
   # shellcheck disable=SC2143
   if [ -z "$(git remote -v | grep 'hg')" ] ; then
-    echo "Initial setup of hg::http://hg.openjdk.java.net/$HG_REPO"
-    git remote add hg hg::http://hg.openjdk.java.net/"$HG_REPO"
+    echo "Initial setup of hg::https://hg.openjdk.java.net/$HG_REPO"
+    git remote add hg hg::https://hg.openjdk.java.net/"$HG_REPO"
   fi
 }
 
