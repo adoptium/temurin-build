b7e7a86d920faf96af84fd9a2ca196b06225e0f4
John Oliver <johno@insightfullogic.com>
2018-10-01 17:45:48 +0100

    Fix logic

--- pipelines/build/build_base_file.groovy
+++ pipelines/build/build_base_file.groovy
@@ -56,11 +56,12 @@ static def buildConfiguration(javaToBuild, variant, configuration, releaseTag, b
 
     buildParams.putAll(getConfigureArgs(configuration, additionalConfigureArgs))
 
-    def buildArgs = configuration.buildArgs;
+    def buildArgs = "";
+    if (configuration.containsKey('buildArgs')) buildArgs += configuration.buildArgs;
     if (additionalBuildArgs != null && additionalBuildArgs.length() > 0) {
         buildArgs += " " + additionalBuildArgs
     }
-    if (configuration.containsKey('buildArgs')) buildParams.put("BUILD_ARGS", buildArgs)
+    buildParams.put("BUILD_ARGS", buildArgs)
 
 
     if (branch != null && branch.length() > 0) {
