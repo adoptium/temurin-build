26b56aef3e239843f289d35a2eb00a0d8124e89e
Martijn Verburg <martijnverburg@gmail.com>
2018-10-10 11:22:18 +0100

    special case the tag for aarch64 java 1.8.0_181-b13

--- git-hg/mercurialToGitWithSubModules.sh
+++ git-hg/mercurialToGitWithSubModules.sh
@@ -124,11 +124,18 @@ TAGS="$@"
 
 function setMercurialRepoAndTagsToRetrieve() {
   case "$OPENJDK_VERSION" in
-     jdk8*) # OpenJDK does not provide this yet. HEAD is interpreted by the
-            # script below to mean tip/latest commit.
+     jdk8*) # If it's the aarch64 port then they have their own special tag name for 181-b13
+            # TODO Exact URL matching is brittle
+            if [ ${HG_REPO} == *"aarch64-port"* ] ; then
+                TAG_181="aarch64-jdk8u181-b13"
+            else
+                TAG_181="jdk8u181-b13"
+            fi
+
+            # HEAD is interpreted by the script below to mean tip/latest commit.
             # Skipping jdk8u152-b16 as it seems to be problematic
             if [ -z "$TAGS" ] ; then
-                TAGS="jdk8u144-b34 jdk8u162-b12 jdk8u172-b11 jdk8u181-b13 HEAD"
+                TAGS="jdk8u144-b34 jdk8u162-b12 jdk8u172-b11 ${TAG_181} HEAD"
             fi;;
      jdk9*) if [ -z "$TAGS" ] ; then
                 TAGS="jdk-9+181 jdk-9.0.1+11 jdk-9.0.3+9 jdk-9.0.4+11 HEAD"
