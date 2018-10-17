f5e85ee480dd72472e6023fa4763b0184fd440c9
John Oliver <johno@insightfullogic.com>
2018-10-17 14:09:56 +0100

    Try to regenerate generated-configure.sh if we have a conflict

--- git-hg/mercurialToGitWithSubModules.sh
+++ git-hg/mercurialToGitWithSubModules.sh
@@ -260,6 +260,31 @@ function checkoutRoot() {
   git filter-branch -f --index-filter 'git rm -r -f -q --cached --ignore-unmatch .hg .hgignore .hgtags get_source.sh' --prune-empty --tag-name-filter cat -- --all
 }
 
+
+function fixGeneratedConfigure() {
+  # Since generated-configure.sh contains a timestamp it is a frequent source of merge conflicts
+  # if we have a conflict, regenerate that file
+  local result=$1
+
+  if [ $result != 0 ]
+  then
+    local num_conflicts=$(git status | grep "both modified:" | wc -l)
+    local is_generated=$(git status | grep "both modified:.*generated-configure.sh" | wc -l)
+
+    if [ $num_conflicts == 1 ] && [ $is_generated == 1 ]
+    then
+      # we have a conflict, regenerate the file
+      ./common/autoconf/autogen.sh
+      git commit -a --no-edit
+    else
+      echo "Merge conflicts"
+      git status
+      exit 1
+    fi
+
+  fi
+}
+
 function fetchRootTagIntoRepo() {
   NEWTAG=$1
 
@@ -271,7 +296,12 @@ function fetchRootTagIntoRepo() {
   git branch --unset-upstream || true
   git checkout master
   git fetch "$REWRITE_WORKSPACE/root"
+
+  set +x
   git merge --allow-unrelated-histories -m "Merge base $NEWTAG" FETCH_HEAD
+  local result=$?
+  set -x
+  fixGeneratedConfigure $result
 }
 
 function fetchModuleTagIntoRepo() {
