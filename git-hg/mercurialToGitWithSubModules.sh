1339b52fdf05a902945d697e335ac6008a628c3b
John Oliver <johno@insightfullogic.com>
2018-10-17 14:17:51 +0100

    remove old tags

--- git-hg/mercurialToGitWithSubModules.sh
+++ git-hg/mercurialToGitWithSubModules.sh
@@ -135,7 +135,7 @@ function setMercurialRepoAndTagsToRetrieve() {
             # HEAD is interpreted by the script below to mean tip/latest commit.
             # Skipping jdk8u152-b16 as it seems to be problematic
             if [ -z "$TAGS" ] ; then
-                TAGS="jdk8u144-b34 jdk8u162-b12 jdk8u172-b11 ${TAG_181} jdk8u192-b12 HEAD"
+                TAGS="jdk8u192-b12 HEAD"
             fi;;
      jdk9*) if [ -z "$TAGS" ] ; then
                 TAGS="jdk-9+181 jdk-9.0.1+11 jdk-9.0.3+9 jdk-9.0.4+11 HEAD"
