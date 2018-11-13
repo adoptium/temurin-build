b9c010a91f4003831c96fe7daae7f879d183eda7
Stewart Addison <sxa@uk.ibm.com>
2018-11-13 18:51:42 +0000

    Do not override JDK10_BOOT_DIR if already defined

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -40,12 +40,14 @@ if [ "${JAVA_TO_BUILD}" == "${JDK11_VERSION}" ] || [ "${JAVA_TO_BUILD}" == "${JD
 then
     export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-cxxflags=-mmacosx-version-min=10.8"
 
+  if [ ! -d "$JDK10_BOOT_DIR" ]; then
     export JDK10_BOOT_DIR="$PWD/jdk-10"
     if [ ! -d "$JDK10_BOOT_DIR/bin" ]; then
       mkdir -p "$JDK10_BOOT_DIR"
       wget -q -O - 'https://api.adoptopenjdk.net/v2/binary/releases/openjdk10?os=mac&release=latest' | tar xpzf - --strip-components=2 -C "$JDK10_BOOT_DIR"
     fi
-    export JDK_BOOT_DIR=$JDK10_BOOT_DIR
+  fi
+  export JDK_BOOT_DIR=$JDK10_BOOT_DIR
 fi
 
 if [ "${VARIANT}" == "openj9" ]; then
