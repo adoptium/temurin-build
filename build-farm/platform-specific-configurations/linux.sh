98ced2677d526211f4c7a30213bdef66f0ddd415
Stewart Addison <sxa@uk.ibm.com>
2019-01-03 16:50:57 +0000

    Add type=jdk parameter to JDK10 download API call

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -77,7 +77,7 @@ then
         downloadArch="${ARCHITECTURE}"
         [ "$downloadArch" == "arm" ] && downloadArch="arm32"
         mkdir -p "$JDK10_BOOT_DIR"
-        wget -q -O - "https://api.adoptopenjdk.net/v2/binary/releases/openjdk10?os=linux&release=latest&arch=${downloadArch}" | tar xpzf - --strip-components=2 -C "$JDK10_BOOT_DIR"
+        wget -q -O - "https://api.adoptopenjdk.net/v2/binary/nightly/openjdk10?os=linux&release=latest&arch=${downloadArch}&type=jdk" | tar xpzf - --strip-components=2 -C "$JDK10_BOOT_DIR"
       fi
     fi
     export JDK_BOOT_DIR=$JDK10_BOOT_DIR
