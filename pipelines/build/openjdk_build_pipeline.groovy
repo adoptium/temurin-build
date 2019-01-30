19e9457ad2bc689009be2956b3c1fad3a2249426
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-01-30 13:45:05 +0000

    Proposed fix to adding additional tag to test name (#879)
    * Proposed fix to adding additional tag to test name
    
    * Remove import
    
    * Fix from review
    
    * Move additional name tag to the end of the file name

--- pipelines/build/openjdk_build_pipeline.groovy
+++ pipelines/build/openjdk_build_pipeline.groovy
@@ -61,7 +61,10 @@ def determineTestJobName(config, testType) {
     def jobName = "openjdk${number}_${variant}_${testType}_${arch}_${os}"
 
     if (config.parameters.containsKey('ADDITIONAL_FILE_NAME_TAG')) {
-        jobName += "_${config.parameters.ADDITIONAL_FILE_NAME_TAG}"
+        switch (config.parameters.ADDITIONAL_FILE_NAME_TAG) {
+            case ~/.*linuxXL.*/: jobName += "_linuxXL"; break;
+            case ~/.*macosXL.*/: jobName += "_macosXL"; break;
+        }
     }
     return "${jobName}"
 }
