dd24e4020ff9704647d18a7efef0c3ecf2bb4b98
Simon Rushton <rushton@uk.ibm.com>
2019-02-27 13:33:12 +0000

    Remove --with-milestone=fcs for jdk HEAD builds

--- sbin/build.sh
+++ sbin/build.sh
@@ -154,7 +154,7 @@ configuringVersionStringParameter()
   echo "OpenJDK repo tag is ${openJdkVersion}"
 
   # --with-milestone=fcs deprecated at jdk11, removed at jdk12
-  if [ "${BUILD_CONFIG[OPENJDK_CORE_VERSION]}" != "${JDK12_CORE_VERSION}" ]; then
+  if [ "${BUILD_CONFIG[OPENJDK_CORE_VERSION]}" != "${JDK12_CORE_VERSION}" && "${BUILD_CONFIG[OPENJDK_CORE_VERSION]}" != "${JDKHEAD_CORE_VERSION}" ]; then
     addConfigureArg "--with-milestone=" "fcs"
   fi
 
