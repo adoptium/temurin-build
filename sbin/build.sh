8786d44fac39a51326ed47c8d982141dfe8ec452
Martijn Verburg <martijnverburg@gmail.com>
2019-03-26 13:11:13 +0000

    remove extra whitespace

--- sbin/build.sh
+++ sbin/build.sh
@@ -524,7 +524,7 @@ getFirstTagFromOpenJDKGitRepo()
 {
     git fetch --tags "${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}"
     revList=$(git rev-list --tags --topo-order --max-count=$GIT_TAGS_TO_SEARCH)
-    firstMatchingNameFromRepo=$(git describe --tags $revList  | grep jdk | grep -v openj9 | grep -v "\-ga" | head -1)
+    firstMatchingNameFromRepo=$(git describe --tags $revList | grep jdk | grep -v openj9 | grep -v "\-ga" | head -1)
     # this may not find the correct tag if there are multiples on the commit so find commit
     # that contains this tag and then use `git tag` to find the real tag
     revList=$(git rev-list -n 1 $firstMatchingNameFromRepo --)
