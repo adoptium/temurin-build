72c16d46b47c987138f16aa5ec893cf6f2e4fc96
George Adams <georgeadams1995@gmail.com>
2019-07-03 11:05:09 +0100

    add 10u config file

---
+++ pipelines/jobs/configurations/jdk10.groovy
@@ -0,0 +1,47 @@
+targetConfigurations = [
+        "x64Mac"      : [
+                "hotspot",
+                "openj9"
+        ],
+        "x64MacXL"    : [
+                "openj9"
+        ],
+        "x64Linux"    : [
+                "hotspot",
+                "openj9"
+        ],
+        "x64Windows"  : [
+                "hotspot",
+                "openj9"
+        ],
+        "x64WindowsXL"  : [
+                "openj9"
+        ],
+        "x32Windows"  : [
+                "hotspot"
+        ],
+        "ppc64Aix"    : [
+                "hotspot",
+                "openj9"
+        ],
+        "ppc64leLinux": [
+                "hotspot",
+                "openj9"
+        ],
+        "s390xLinux"  : [
+                "hotspot",
+                "openj9"
+        ],
+        "aarch64Linux": [
+                "hotspot",
+                "openj9"
+        ],
+        "arm32Linux"  : [
+                "hotspot"
+        ],
+        "linuxXL"     : [
+                "openj9"
+        ]
+]
+
+return this
