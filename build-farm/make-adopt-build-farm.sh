808b08fe2aefc005cf53f6cc1deb28a9b323ffc0
Severin Gehwolf <jerboaa@gmail.com>
2020-01-22 14:48:20 +0100

    Add jdk 14 build pipeline (#1485)
    HotSpot only for now

--- build-farm/make-adopt-build-farm.sh
+++ build-farm/make-adopt-build-farm.sh
@@ -63,7 +63,8 @@ case "${JDK_BOOT_VERSION}" in
       "11")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK11_BOOT_DIR}";;
       "12")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK12_BOOT_DIR}";;
       "13")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK13_BOOT_DIR}";;
-      *)      export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK14_BOOT_DIR}";;
+      "14")   export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK14_BOOT_DIR}";;
+      *)      export JDK_BOOT_DIR="${JDK_BOOT_DIR:-$JDK15_BOOT_DIR}";;
 esac
 
 if [ ! -d "${JDK_BOOT_DIR}" ]
---
+++ pipelines/jobs/configurations/jdk14.groovy
@@ -0,0 +1,22 @@
+targetConfigurations = [
+        "x64Mac"      : [
+                "hotspot"
+        ],
+        "x64Linux"    : [
+                "hotspot"
+        ],
+        "x64Windows"  : [
+                "hotspot"
+        ],
+        "ppc64leLinux": [
+                "hotspot"
+        ],
+        "s390xLinux"  : [
+                "hotspot"
+        ],
+        "aarch64Linux": [
+                "hotspot"
+        ]
+]
+
+return this
