1d282ee37179c0f5a251b10264ec7e6c0850310d
Severin Gehwolf <jerboaa@gmail.com>
2020-01-27 09:54:43 +0100

    Fix java -version diagnostic output (#1493)
    With later JDK versions 'java -version' output
    goes to the error stream (fd 2). Account for that
    in diagnostics so that "BOOT JDK: " is properly
    pre-fixed.
    
    Signed-off-by: Severin Gehwolf <sgehwolf@redhat.com>

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -99,7 +99,7 @@ if [ "$JAVA_FEATURE_VERSION" -gt 11 ]; then
       fi
     fi
     export JDK_BOOT_DIR="$(eval echo "\$$BOOT_JDK_VARIABLE")"
-    $JDK_BOOT_DIR/bin/java -version | sed 's/^/BOOT JDK: /'
+    "$JDK_BOOT_DIR/bin/java" -version 2>&1 | sed 's/^/BOOT JDK: /'
 fi
 
 # Any version above 10
