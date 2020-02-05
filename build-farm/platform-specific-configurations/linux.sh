2800cfffebe616a61d5cdcbcd45c5bdfca04ab07
Stewart Addison <sxa@uk.ibm.com>
2020-02-05 16:33:00 +0000

    env: tolerate gcc 7.5 in /usr/local/gcc bin
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -125,7 +125,17 @@ fi
 # Any version above 10
 if [ "$JAVA_FEATURE_VERSION" -gt 10 ] || [ "${VARIANT}" == "${BUILD_VARIANT_OPENJ9}" ]; then
     # If we have the RedHat devtoolset 7 installed, use gcc 7 from there, else /usr/local/gcc/bin
-    if [ -r /opt/rh/devtoolset-7/root/usr/bin ]; then
+    if [ -r /usr/local/gcc/bin/gcc7.5 ]; then
+      export PATH=/usr/local/gcc/bin:$PATH
+      [ -r /usr/local/gcc/bin/gcc7.5 ] && export CC=/usr/local/gcc/bin/gcc7.5
+      [ -r /usr/local/gcc/bin/g++7.5 ] && export CXX=/usr/local/gcc/bin/g++7.5
+      export LD_LIBRARY_PATH=/usr/local/gcc/lib64:/usr/local/gcc/lib
+    elif [ -r /usr/local/gcc/bin/gcc-7.5 ]; then
+      export PATH=/usr/local/gcc/bin:$PATH
+      [ -r /usr/local/gcc/bin/gcc-7.5 ] && export CC=/usr/local/gcc/bin/gcc-7.5
+      [ -r /usr/local/gcc/bin/g++-7.5 ] && export CXX=/usr/local/gcc/bin/g++-7.5
+      export LD_LIBRARY_PATH=/usr/local/gcc/lib64:/usr/local/gcc/lib
+    elif [ -r /opt/rh/devtoolset-7/root/usr/bin ]; then
       export PATH=/opt/rh/devtoolset-7/root/usr/bin:$PATH
       [ -r /opt/rh/devtoolset-7/root/usr/bin/gcc ] && export CC=/opt/rh/devtoolset-7/root/usr/bin/gcc
       [ -r /opt/rh/devtoolset-7/root/usr/bin/g++ ] && export CXX=/opt/rh/devtoolset-7/root/usr/bin/g++
