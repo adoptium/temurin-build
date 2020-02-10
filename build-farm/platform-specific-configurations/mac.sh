158732049e14d6242f295360236334cf93154874
Stewart X Addison <sxa555@users.noreply.github.com>
2020-02-10 18:01:56 +0000

    Revert "Removed erroneous tags (#1530)" (#1536)
    This reverts commit 0cba20148cb3e02f2a830f2f912be1b4febaf386.

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -83,7 +83,7 @@ if [ "$JAVA_FEATURE_VERSION" -gt 11 ]; then
         # for the fallback mechanism, as downloading of the GA binary might
         # fail.
         set +e
-        wget -q -O - "${apiURL}" | tar xpzf --strip-components=1 -C "$bootDir"
+        wget -q -O - "${apiURL}" | tar xpzf - --strip-components=1 -C "$bootDir"
         retVal=$?
         set -e
         if [ $retVal -ne 0 ]; then
@@ -94,7 +94,7 @@ if [ "$JAVA_FEATURE_VERSION" -gt 11 ]; then
           # shellcheck disable=SC2034
           releaseType="ea"
           apiURL=$(eval echo ${apiUrlTemplate})
-          wget -q -O - "${apiURL}" | tar xpzf --strip-components=1 -C "$bootDir"
+          wget -q -O - "${apiURL}" | tar xpzf - --strip-components=1 -C "$bootDir"
         fi
       fi
     fi
