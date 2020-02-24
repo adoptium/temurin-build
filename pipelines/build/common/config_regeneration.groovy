8fc5bae0c826e1a2feb1c31034d0982057b7b25b
Morgan Davies <morgan.davies@ibm.com>
2020-02-24 14:51:46 +0000

    see if jenkins will accept a linked hash map
    Signed-off-by: Morgan Davies <morgan.davies@ibm.com>

--- pipelines/build/common/config_regeneration.groovy
+++ pipelines/build/common/config_regeneration.groovy
@@ -109,10 +109,11 @@ class Regeneration implements Serializable {
     ]
 
     Map<String, IndividualBuildConfig> jobConfigurations = [:]
-    
+
     jobConfigurations["linux-x64-hotspot"] = buildConfiguration(platformConfig, "hotspot")
 
-    Map<String, ?> params = platformConfig.toMap().clone() as Map
+    //Map<String, ?> params = platformConfig.toMap().clone() as Map
+    Map<String, ?> params = platformConfig
 
     def jdkVersion = "openjdkxx-pipeline" // Based off the openjdk11_pipeline.groovy build config
 
