23588e55d30ca05cbbb85a8d34728910beb9c4f7
Stewart Addison <sxa@uk.ibm.com>
2020-02-25 21:18:09 +0000

    macos: Disable codesigning attempts on JDK13
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -50,8 +50,8 @@ else
   fi
 fi
 
-# The configure option '--with-macosx-codesign-identity' is supported in Java 11 and higher versions.
-if [ "$JAVA_FEATURE_VERSION" -ge 11 ]
+# The configure option '--with-macosx-codesign-identity' is supported in JDK11 and JDK14+
+if [ "$JAVA_FEATURE_VERSION" -eq 11 ] || [ "$JAVA_FEATURE_VERSION" -ge 14 ]
 then
   export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/"
   # Login to KeyChain
