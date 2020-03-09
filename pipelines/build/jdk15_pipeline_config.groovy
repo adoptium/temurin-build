361442f6b6749e1919abd1fc2bd4d4f01bed58e9
Morgan Davies <morgan.davies@ibm.com>
2020-03-09 18:05:46 +0000

    instantiate as map, return class
    Signed-off-by: Morgan Davies <morgan.davies@ibm.com>

--- pipelines/build/jdk15_pipeline_config.groovy
+++ pipelines/build/jdk15_pipeline_config.groovy
@@ -1,80 +1,75 @@
-class Config implements Serializable {
+class Config {
   Map<String, Map<String, ?>> buildConfigurations = [
-          x64Mac    : [
-                  os                  : 'mac',
-                  arch                : 'x64',
-                  additionalNodeLabels: 'macos10.14',
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+          "x64Mac"    : [
+                  "os"                  : 'mac',
+                  "arch"                : 'x64',
+                  "additionalNodeLabels": 'macos10.14',
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                   ]
           ],
 
-          x64Linux  : [
-                  os                  : 'linux',
-                  arch                : 'x64',
-                  additionalNodeLabels: 'centos6',
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.external', 'special.functional']
+          "x64Linux"  : [
+                  "os"                  : 'linux',
+                  "arch"                : 'x64',
+                  "additionalNodeLabels": 'centos6',
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.external', 'special.functional']
                   ],
-                  configureArgs        : '--disable-ccache'
+                  "configureArgs"        : '--disable-ccache'
           ],
 
           // Currently we have to be quite specific about which windows to use as not all of them have freetype installed
-          x64Windows: [
-                  os                  : 'windows',
-                  arch                : 'x64',
-                  additionalNodeLabels: [
-                          hotspot: 'win2012&&vs2017'
+          "x64Windows": [
+                  "os"                  : 'windows',
+                  "arch"                : 'x64',
+                  "additionalNodeLabels": [
+                          "hotspot": 'win2012&&vs2017'
                   ],
-                  buildArgs : [
-                          hotspot : '--jvm-variant client,server'
+                  "buildArgs" : [
+                          "hotspot" : '--jvm-variant client,server'
                   ],
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.perf', 'sanity.system', 'extended.system']
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.perf', 'sanity.system', 'extended.system']
                   ]
           ],
 
-          s390xLinux    : [
-                  os                  : 'linux',
-                  arch                : 's390x',
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+          "s390xLinux"    : [
+                  "os"                  : 'linux',
+                  "arch"                : 's390x',
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                   ],
-                  configureArgs        : '--disable-ccache'
+                  "configureArgs"        : '--disable-ccache'
           ],
 
-          ppc64leLinux    : [
-                  os                  : 'linux',
-                  arch                : 'ppc64le',
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+          "ppc64leLinux"    : [
+                  "os"                  : 'linux',
+                  "arch"                : 'ppc64le',
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                   ],
-                  configureArgs       : '--disable-ccache'
+                  "configureArgs"       : '--disable-ccache'
 
           ],
 
-          aarch64Linux    : [
-                  os                  : 'linux',
-                  arch                : 'aarch64',
-                  additionalNodeLabels: 'centos7',
-                  test                : [
-                          nightly: false,
-                          release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+          "aarch64Linux"    : [
+                  "os"                  : 'linux',
+                  "arch"                : 'aarch64',
+                  "additionalNodeLabels": 'centos7',
+                  "test"                : [
+                          "nightly": false,
+                          "release": ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                   ]
           ],
 
-  ];
-
-  public Map getBuildConfigurations() {
-    return buildConfigurations;
-  }
+  ]
 
 }
 
-Config config = new Config();
-return config.getBuildConfigurations();
+return Config
