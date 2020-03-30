f1344a237ecda3247e7d6d9c7b360610d5f75959
Will Parker <william.parker1@ibm.com>
2020-03-30 16:16:41 +0100

    Windows: Correct OpenSSL path

--- build-farm/platform-specific-configurations/windows.sh
+++ build-farm/platform-specific-configurations/windows.sh
@@ -78,8 +78,8 @@ then
   if [ "${VARIANT}" == "${BUILD_VARIANT_OPENJ9}" ]
   then
     # If statements to ensure build machines don't break if they're yet to have openssl-1.1.1e put on them
-    if [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_32 ]; then
-      export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_32 --enable-openssl-bundling"
+    if [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_32-VS2013 ]; then
+      export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_32-VS2013 --enable-openssl-bundling"
     else
       export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_32 --enable-openssl-bundling"
     fi
@@ -132,8 +132,8 @@ then
       export PATH="$PATH:/c/cygwin64/bin"
       export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/cygdrive/c/openjdk/freemarker.jar --disable-ccache"
       # If statements to ensure build machines don't break if they're yet to have openssl-1.1.1e put on them
-      if [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64 ]; then
-        export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64 --enable-openssl-bundling"
+      if [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64-VS2013 ]; then
+        export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64-VS2013 --enable-openssl-bundling"
       else
         export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64 --enable-openssl-bundling"
       fi
@@ -152,8 +152,8 @@ then
       # If statements to ensure build machines don't break if they're yet to have openssl-1.1.1e put on them
       if [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64-VS2017 ]; then
         export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/cygdrive/c/openjdk/freemarker.jar --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1e-x86_64-VS2017 --enable-openssl-bundling"
-      elif [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64.VS2017 ]; then
-        export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/cygdrive/c/openjdk/freemarker.jar --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64.VS2017 --enable-openssl-bundling"	
+      elif [ -d /cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64-VS2017 ]; then
+        export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/cygdrive/c/openjdk/freemarker.jar --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64-VS2017 --enable-openssl-bundling"	
       else	
         export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/cygdrive/c/openjdk/freemarker.jar --with-openssl=/cygdrive/c/openjdk/OpenSSL-1.1.1d-x86_64 --enable-openssl-bundling"
       fi
