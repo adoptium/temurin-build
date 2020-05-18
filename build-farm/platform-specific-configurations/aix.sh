b1265905356543a06f71bec2fa409c03b6d964c8
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2020-05-18 19:15:37 +0100

    Strip java directories from the AIX LIBPATH (#1747)
    * Strip java directories from the AIX LIBPATH
    
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>
    
    * Squash AIX LIBPATH so that it starts empty at start of build
    
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>

--- build-farm/platform-specific-configurations/aix.sh
+++ build-farm/platform-specific-configurations/aix.sh
@@ -28,8 +28,9 @@ fi
 # shellcheck source=sbin/common/constants.sh
 source "$SCRIPT_DIR/../../sbin/common/constants.sh"
 export PATH="/opt/freeware/bin:/usr/local/bin:/opt/IBM/xlC/13.1.3/bin:/opt/IBM/xlc/13.1.3/bin:$PATH"
-# Without this, java adds /usr/lib to the LIBPATH of anything it forks which breaks linkage
-export LIBPATH="/opt/freeware/lib:$LIBPATH"
+# Without this, java adds /usr/lib to the LIBPATH and it's own library
+# directories of anything it forks which breaks linkage
+export LIBPATH=/opt/freeware/lib
 export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-cups-include=/opt/freeware/include"
 
 # Any version below 11
