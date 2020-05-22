1d5fe2c0faff689baa72f68704622807c8b200b9
Stewart Addison <sxa@uk.ibm.com>
2020-05-22 17:29:10 +0100

    Use OpenJ9 nightly as build JDK for RISC-V
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -151,13 +151,19 @@ fi
 
 if [ "${ARCHITECTURE}" == "riscv" ]
 then
+	echo RISCV cross-compilation ... Downloading latest nightly OpenJ9/x64 as build JDK
+	export BUILDJDK=$WORKSPACE/buildjdk
+	rm -r "$BUILDJDK"
+	mkdir "$BUILDJDK"
+	wget -O - "https://api.adoptopenjdk.net/v3/binary/latest/${JAVA_FEATURE_VERSION}/ea/linux/x64/jdk/openj9/normal/adoptopenjdk" | tar xpzf - --strip-components=1 -C "$BUILDJDK"
+	"$BUILDJDK/bin/java" -version 2>&1 | sed 's/^/CROSSBUILD JDK > /g'
 	export RISCV64=/opt/riscv_toolchain_linux
 	export LD_LIBRARY_PATH=$RISCV64/lib64
 	export PATH="$RISCV64/bin:$PATH"
 	# riscv has to use a cross compiler
 	export CC=$RISCV64/bin/riscv64-unknown-linux-gnu-gcc
 	export CXX=$RISCV64/bin/riscv64-unknown-linux-gnu-g++
-	CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --disable-ddr --openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root --with-boot-jdk=$JDK_BOOT_DIR --with-build-jdk=$JDK_BOOT_DIR"
+	CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --disable-ddr --openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root --with-boot-jdk=$JDK_BOOT_DIR --with-build-jdk=$BUILDJDK"
 	BUILD_ARGS="${BUILD_ARGS} -F"
 fi
 
