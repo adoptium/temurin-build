326fbf8f77bc48d346c4c7c977ae2aed37eb3df5
Morgan Davies <morgan.davies@ibm.com>
2020-06-07 16:38:07 +0100

    Use most_recent_feature_version (#1829)
    * most_recent_feature_release has reverted back to 14 since #1822

--- pipelines/build/common/build_base_file.groovy
+++ pipelines/build/common/build_base_file.groovy
@@ -208,12 +208,12 @@ class Builder implements Serializable {
         if (matcher.matches()) {
             return Integer.parseInt(matcher.group('version'))
         } else if ("jdk".equalsIgnoreCase(javaToBuild.trim())) {
-            // Query the Adopt api to get the "most_recent_feature_release" (currently 15)
+            // Query the Adopt api to get the "most_recent_feature_version" (currently 15)
             def JobHelper = context.library(identifier: 'openjdk-jenkins-helper@master').JobHelper
-            context.println "Querying Adopt Api for the JDK-Head number (most_recent_feature_release)..."
+            context.println "Querying Adopt Api for the JDK-Head number (most_recent_feature_version)..."
 
             response = JobHelper.getAvailableReleases()
-            Integer headVersion = Integer.valueOf(response.most_recent_feature_release)
+            Integer headVersion = Integer.valueOf(response.most_recent_feature_version)
             if (headVersion.equalsIgnoreCase(null)) {
                 context.println "Failure on api connection or parsing."
                 throw new Exception()
