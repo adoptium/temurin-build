524a7c783e5e9c2c40a220bfa23fe7da5e22b416
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2020-06-08 13:51:11 +0100

    If an HTTP download fails in downloadFile, abort rather than fail later (#1784)
    * Make downloadFile verbose
    
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>
    
    * downloadFile: Abort on download failure
    
    Signed-off-by: Stewart Addison <sxa@uk.ibm.com>

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -261,13 +261,12 @@ downloadFile() {
   local url="$2"
 
   echo downloadFile: Saving "url" to "$targetFileName"
-
   
   # Temporary fudge as curl on my windows boxes is exiting with RC=127
   if [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-    wget -O "${targetFileName}" "${url}"
+    wget -O "${targetFileName}" "${url}" || exit 2
   else
-    curl -L -o "${targetFileName}" "${url}"
+    curl --fail -L -o "${targetFileName}" "${url}" || exit 2
   fi
 
   if [ $# -ge 3 ]; then
