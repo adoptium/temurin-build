63f373a43ad73ebcd2e4dfe4dee4f0cbbfefe47e
Morgan Davies <morgan.davies@ibm.com>
2020-06-12 13:33:35 +0100

    Handles u in javaVersion * Renames config files to jdk<version>u * Includes u in regen pipeplines
    Signed-off-by: Morgan Davies <morgan.davies@ibm.com>

--- pipelines/build/regeneration/jdk12_regeneration_pipeline.groovy
+++ pipelines/build/regeneration/jdk12_regeneration_pipeline.groovy
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-String javaVersion = "jdk12"
+String javaVersion = "jdk12u"
 
 node ("master") {
   try {
@@ -24,7 +24,7 @@ node ("master") {
     println "[INFO] Found buildConfigurations:\n$buildConfigurations"
 
     // Load targetConfigurations from config file. This is what is being run in the nightlies
-    load "${WORKSPACE}/pipelines/jobs/configurations/${javaVersion}u.groovy"
+    load "${WORKSPACE}/pipelines/jobs/configurations/${javaVersion}.groovy"
 
     println "[INFO] Found targetConfigurations:\n$targetConfigurations"
 
