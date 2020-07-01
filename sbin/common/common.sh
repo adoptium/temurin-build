5291ab7770f9107cb3f8e35f72b5c4faddf8758a
Morgan Davies <morgan.davies@ibm.com>
2020-07-01 10:00:31 +0100

    Switch timeout value to seconds
    Signed-off-by: Morgan Davies <morgan.davies@ibm.com>

--- sbin/common/common.sh
+++ sbin/common/common.sh
@@ -49,7 +49,7 @@ function setOpenJdkVersion() {
         then
             echo "RETRYWARNING: Query ${retryCount} failed. Retrying in 30 seconds (max retries = ${retryMax})..."
             retryCount=$((retryCount+1)) 
-            sleep 30000
+            sleep 30
         else
             echo "featureNumber FOUND: ${featureNumber}" && break
         fi
