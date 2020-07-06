93fc3c34ed4c5814da1d2c11b84d3906eec9c457
Will Parker <52783864+Willsparker@users.noreply.github.com>
2020-07-06 19:06:09 +0100

    pipeline: Manually delete problematic file for Azure Windows (#1956)
    Co-authored-by: Morgan Davies <morgan.davies@ibm.com>
    
    Co-authored-by: Morgan Davies <morgan.davies@ibm.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -542,9 +542,11 @@ class Build {
             if (cleanWorkspace) {
                 try {
                     if (buildConfig.TARGET_OS == "windows") {
-                        // Softlayer machines struggle to clean themselves
+                        // Windows machines struggle to clean themselves, see:
                         // https://github.com/AdoptOpenJDK/openjdk-build/issues/1855
                         context.sh(script: "rm -rf C:/workspace/openjdk-build/workspace/build/src/build/*/jdk/gensrc")
+                        // https://github.com/AdoptOpenJDK/openjdk-infrastructure/issues/1419
+                        context.sh(script: "rm -rf J:/jenkins/tmp/workspace/build/src/build/*/jdk/gensrc")
                         context.cleanWs notFailBuild: true, disableDeferredWipeout: true, deleteDirs: true
                     } else {
                         context.cleanWs notFailBuild: true
