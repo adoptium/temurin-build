3551b502d68baa9c6aefc08ddb7e9281bafcab74
George Adams <georgeadams1995@gmail.com>
2020-07-16 19:09:26 +0100

    fix mirror script (#1973)
    * fix mirror script
    
    * Update merge.sh
    
    * Update updateRepo.sh
    
    * Update updateRepo.sh
    
    * Update merge.sh
    
    * Update merge.sh

--- git-hg/jdk8u/merge.sh
+++ git-hg/jdk8u/merge.sh
@@ -10,7 +10,11 @@ doInit="false"
 doReset="false"
 doTagging="false"
 doUpdate="false"
-hgRepo="https://hg.openjdk.java.net/jdk8u/jdk8u"
+if [ -z ${HG_REPO} ]; then
+  hgRepo="https://hg.openjdk.java.net/jdk8u/jdk8u"
+else 
+  hgRepo="$HG_REPO"
+fi
 tag="jdk8u172-b08"
 workingBranch="master"
 
@@ -139,7 +143,7 @@ function rebuildLocalRepo() {
     mkdir -p "$REPO"
     cd "$REPO"
     git clone $UPSTREAM_GIT_REPO .
-    git checkout master
+    git checkout master || git checkout -b master
 
     # Step 3 Setup remotes
     addRemotes
