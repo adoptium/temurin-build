1c18980fabd29725ac7c47e0e595ebe825f18c10
George Adams <george.adams@microsoft.com>
2020-09-17 09:52:59 +0100

    Update build.sh

--- sbin/build.sh
+++ sbin/build.sh
@@ -707,6 +707,7 @@ moveFreetypeLib() {
 
   # codesign freetype before it is bundled
   if [ ! -z "${BUILD_CONFIG[MACOSX_CODESIGN_IDENTITY]}" ]; then
+    # test if codesign certificate is usable
     if touch test && codesign --sign "Developer ID Application: London Jamocha Community CIC" test && rm -rf test; then
       ENTITLEMENTS="$WORKSPACE/entitlements.plist"
       codesign --entitlements "$ENTITLEMENTS" --options runtime --timestamp --sign "${BUILD_CONFIG[MACOSX_CODESIGN_IDENTITY]}" "${SOURCE_LIB_NAME}"
