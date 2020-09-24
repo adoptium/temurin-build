773c2ebc6102a1d09d194c19eb236edda7cb7e50
sophia-guo <sophia.gwf@gmail.com>
2020-09-24 08:25:48 -0400

    trigger with standard set (#2024)
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>
    
    Co-authored-by: Sophia Guo <sophia.gwf@gail.com>

--- pipelines/build/common/config_regeneration.groovy
+++ pipelines/build/common/config_regeneration.groovy
@@ -35,6 +35,7 @@ class Regeneration implements Serializable {
 
     private final def jenkinsBuildRoot
     private String javaToBuild
+    private final List<String> defaultTestList = ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.external']
 
     private final String excludedConst = "EXCLUDED"
 
@@ -168,14 +169,15 @@ class Regeneration implements Serializable {
     * @param configuration
     */
     List<String> getTestList(Map<String, ?> configuration) {
-        if (configuration.containsKey("test")) {
+        List<String> testList = []
+        if (configuration.containsKey("test") && configuration.get("test")) {
             if (isMap(configuration.test)) {
-                return (configuration.test as Map).get("nightly") as List<String> // no need to check for release
-            } else {
-                return configuration.test as List<String>
+                testList = (configuration.test as Map).get("nightly") as List<String> // no need to check for release
             }
+            testList = defaultTestList
         }
-        return []
+        testList.unique()
+        return testList
     }
 
     /*
--- pipelines/jobs/configurations/jdk16_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk16_pipeline_config.groovy
@@ -5,8 +5,8 @@ class Config16 {
                 arch                : 'x64',
                 additionalNodeLabels: 'macos10.14',
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                 ],
                 configureArgs       : '--enable-dtrace'
         ],
@@ -19,8 +19,8 @@ class Config16 {
                         openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
                 ],
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.external', 'special.functional']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.external', 'special.functional']
                 ],
                 configureArgs       : [
                         "openj9"      : '--enable-dtrace --enable-jitserver',
@@ -36,8 +36,8 @@ class Config16 {
                         hotspot: 'win2012&&vs2017'
                 ],
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.perf', 'sanity.system', 'extended.system']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.perf', 'sanity.system', 'extended.system']
                 ]
         ],
 
@@ -61,8 +61,8 @@ class Config16 {
                         openj9:  'xlc16&&aix715'
                 ],
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system']
                 ]
         ],
 
@@ -71,8 +71,8 @@ class Config16 {
                 os                  : 'linux',
                 arch                : 's390x',
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                 ],
                 configureArgs       : '--enable-dtrace'
         ],
@@ -81,8 +81,8 @@ class Config16 {
                 os                  : 'linux',
                 arch                : 'ppc64le',
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+                        nightly: [],
+                        weekly: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                 ],
                 configureArgs       : [
                         "hotspot"     : '--enable-dtrace',
@@ -96,8 +96,8 @@ class Config16 {
                 arch                : 'aarch64',
                 dockerImage         : 'adoptopenjdk/centos7_build_image',
                 test                : [
-                        nightly: false,
-                        release: ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+                        nightly: [],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
                 ],
                 configureArgs       : '--enable-dtrace'
         ],
