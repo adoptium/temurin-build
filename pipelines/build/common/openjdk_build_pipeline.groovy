53303cc9a72da4d3626fbbd43941ed1a4167f0ed
Morgan Davies <morgandavies2020@gmail.com>
2021-01-18 14:52:15 +0000

    try checking if it is a true string

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -951,7 +951,7 @@ class Build {
                         context.timeout(time: buildTimeouts.BUILD_JDK_TIMEOUT, unit: "HOURS") {
                             def repoHandler = new RepoHandler(context, USER_REMOTE_CONFIGS)
 
-                            if (buildConfig.USE_ADOPT_BASH_SCRIPTS == true) {
+                            if (buildConfig.USE_ADOPT_BASH_SCRIPTS == "true") {
                                 context.println "[CHECKOUT] Checking out to AdoptOpenJDK/openjdk-build to use their bash scripts..."
                                 repoHandler.checkoutAdopt()
                                 context.sh(script: "./build-farm/make-adopt-build-farm.sh")
