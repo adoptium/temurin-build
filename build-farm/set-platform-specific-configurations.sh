4a2c14fa29c1460f964b9c8825bc97b10a663097
Morgan Davies <morgandavies2020@gmail.com>
2021-01-26 19:56:17 +0000

    Add in an additional check for platforms on a failed direct link

--- build-farm/set-platform-specific-configurations.sh
+++ build-farm/set-platform-specific-configurations.sh
@@ -41,15 +41,27 @@ echo "Attempting to download user platform configuration file from https://raw.g
 # make-adopt-build-farm.sh has 'set -e'. We need to disable that for the fallback mechanism, as downloading might fail
 set +e
 curl "https://raw.githubusercontent.com/${PLATFORM_CONFIG_LOCATION}" > "${PLATFORM_CONFIG_FILEPATH}"
-
 ret=$?
+# A download will succeed if the location is a directory
+fileContents=$(cat $PLATFORM_CONFIG_FILEPATH)
 set -e
 
-if [ $ret -ne 0 ]
+if [[ $ret -ne 0 || $fileContents == "404: Not Found" ]]
 then
-    # If there is no user platform config, use adopt's as a default instead
-    echo "Failed to download a user platform configuration file. Downloading Adopt's platform configuration file instead from https://raw.githubusercontent.com/${ADOPT_PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh"
-    curl "https://raw.githubusercontent.com/${ADOPT_PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh" > "${PLATFORM_CONFIG_FILEPATH}"
+    # Check to make sure that the a OS file doesn't exist if we can't find a config file from the direct link
+    echo "Failed to a user configuration file, ${PLATFORM_CONFIG_LOCATION} is likely a directory. Trying ${PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh"
+    set +e
+    curl "https://raw.githubusercontent.com/${PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh" > "${PLATFORM_CONFIG_FILEPATH}"
+    ret=$?
+    fileContents=$(cat $PLATFORM_CONFIG_FILEPATH)
+    set -e
+
+    if [[ $ret -ne 0 || $fileContents == "404: Not Found" ]]
+    then
+        # If there is no user platform config, use adopt's as a default instead
+        echo "Failed to download a user platform configuration file. Downloading Adopt's platform configuration file instead from https://raw.githubusercontent.com/${ADOPT_PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh"
+        curl "https://raw.githubusercontent.com/${ADOPT_PLATFORM_CONFIG_LOCATION}/${OPERATING_SYSTEM}.sh" > "${PLATFORM_CONFIG_FILEPATH}"
+    fi
 fi
 
 echo "Config file downloaded successfully to ${PLATFORM_CONFIG_FILEPATH}"
