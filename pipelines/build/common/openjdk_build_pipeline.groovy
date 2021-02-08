8c5be7e0b7232620aa1617068cea282444639b6c
George Adams <georgeadams1995@gmail.com>
2021-02-08 16:14:21 +0000

    try again

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -966,7 +966,7 @@ class Build {
                 context.withEnv(envVars) {
                     try {
                         context.timeout(time: buildTimeouts.BUILD_JDK_TIMEOUT, unit: "HOURS") {
-                            updateGithubCommitStatus("PENDING", "Build Started", DOWNSTREAM_JOB_URL)
+                            updateGithubCommitStatus("PENDING", "Build Started", "https://google.com")
                             if (useAdoptShellScripts) {
                                 context.println "[CHECKOUT] Checking out to AdoptOpenJDK/openjdk-build to use their bash scripts..."
                                 repoHandler.checkoutAdopt()
