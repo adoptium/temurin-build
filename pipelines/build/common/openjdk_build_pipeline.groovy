bc6bf0ac5d382b473cbe655a323798f011ace49f
George Adams <georgeadams1995@gmail.com>
2021-02-08 14:21:04 +0000

    try status

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1107,6 +1107,16 @@ class Build {
                     */
                     context.library(identifier: 'openjdk-jenkins-helper@master')
 
+                    void setBuildStatus(String message, String state) {
+                        step([
+                            $class: "GitHubCommitStatusSetter",
+                            reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/adoptopenjdk/openjdk-build"],
+                            contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/build-status"],
+                            errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
+                            statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+                        ]);
+                    }
+
                     if (buildConfig.DOCKER_IMAGE) {
                         // Docker build environment
                         def label = buildConfig.NODE_LABEL + "&&dockerBuild"
