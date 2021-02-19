db94864acc09d6baf1405e4663d6bda3ae89913d
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2021-02-19 22:44:21 +0000

    macos: Use JDK8 for bootstrapping JDK8 (#2483)
    * macos: Use JDK8 for bootstrapping JDK8
    
    Signed-off-by: Stewart X Addison <sxa@redhat.com>
    Co-authored-by: Martijn Verburg <martijnverburg@gmail.com>

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -66,7 +66,12 @@ fi
 
 sudo xcode-select --switch "${XCODE_SWITCH_PATH}"
 
-BOOT_JDK_VERSION="$((JAVA_FEATURE_VERSION-1))"
+# Adopt does not have builds of OpenJDK 7, OpenJDK 8 can boot itself just fine.
+if [ "${JAVA_FEATURE_VERSION" = "8" ]; then
+  BOOT_JDK_VERSION="${JAVA_FEATURE_VERSION}"
+else
+  BOOT_JDK_VERSION="$((JAVA_FEATURE_VERSION-1))"
+fi
 BOOT_JDK_VARIABLE="JDK$(echo $BOOT_JDK_VERSION)_BOOT_DIR"
 if [ ! -d "$(eval echo "\$$BOOT_JDK_VARIABLE")" ]; then
   bootDir="$PWD/jdk-$BOOT_JDK_VERSION"
