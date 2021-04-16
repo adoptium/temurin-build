3b2b189afcdacc76361078d01a01899dac322df9
George Adams <georgeadams1995@gmail.com>
2021-04-16 10:51:23 +0100

    macOS: prevent m1 builds from cross compiling x64 (#2573)

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -58,6 +58,8 @@ else
     if [ "${ARCHITECTURE}" == "x64" ]; then
       # We can only target 10.9 on intel macs
       export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-cxxflags=-mmacosx-version-min=10.9"
+    elif [ "${ARCHITECTURE}" == "arm64" ]; then
+      export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --openjdk-target=aarch64-apple-darwin"
     fi
   fi
 fi
