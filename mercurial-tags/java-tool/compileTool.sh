054e1b94478515d667f62b9054b74f4fdfe9ca9c
Morgan Davies <morgandavies2020@gmail.com>
2021-05-04 16:31:09 +0100

    Delete java-tool
    * Hasn't been touched in 3 years
    * Generating lots of errors

--- mercurial-tags/java-tool/compileTool.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo ""
-echo "*********** Compiling the mercurial tracker tool... "
-cd "${SCRIPT_DIR}" || true
-javac MercurialTracker.java
-echo "*********** Finished compiling ******************** "
