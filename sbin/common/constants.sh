f7cbfe539733f4c0d14fc8a42002c49c69f9ea95
George Adams <georgeadams1995@gmail.com>
2021-08-24 13:42:17 +0100

    bump JDK17 to JDK17u

--- sbin/common/constants.sh
+++ sbin/common/constants.sh
@@ -24,7 +24,7 @@ export JDK13_VERSION="jdk13u";
 export JDK14_VERSION="jdk14u";
 export JDK15_VERSION="jdk15u";
 export JDK16_VERSION="jdk16u";
-export JDK17_VERSION="jdk17";
+export JDK17_VERSION="jdk17u";
 export JDKHEAD_VERSION="jdk";
 
 export JDK8_CORE_VERSION="jdk8";
