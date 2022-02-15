e81354e4ec621ed44790eebc5eee03b28166792d
Sehrish Aslam <66127574+SehrishHussain@users.noreply.github.com>
2022-02-15 14:45:15 +0500

    Fixed Lint errors

--- cyclonedx-lib/src/temurin/sbom/TemurinGenSBOM.java
+++ cyclonedx-lib/src/temurin/sbom/TemurinGenSBOM.java
@@ -1,4 +1,4 @@
-/*# Licensed under the Apache License, Version 2.0 (the "License");
+/**# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -115,7 +115,7 @@ public final class TemurinGenSBOM {
                 writeJSONfile(bom, fileName);
                 break;
             default:
-                System.out.println("Please enter a command.");    
+                System.out.println("Please enter a command.");
         }
     }
 
