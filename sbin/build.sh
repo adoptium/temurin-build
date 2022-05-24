d90dc655407308c7be5dfd95a1552f6c06095b8f
Wen Zhou <wenzhou@redhat.com>
2022-05-24 10:26:04 +0200

    fix: missing JAVA_HOME set for Solaris build when call sbom
    Fixes: #2942

--- sbin/build.sh
+++ sbin/build.sh
@@ -640,6 +640,8 @@ buildCyclonedxLib() {
     javaHome=${JDK8_BOOT_DIR}
   elif [ ${JDK11_BOOT_DIR+x} ] && [ -d "${JDK11_BOOT_DIR}" ]; then
     javaHome=${JDK11_BOOT_DIR}
+  elif [ ${JDK_BOOT_DIR+x} ] && [ -d "${JDK_BOOT_DIR}" ]; then # fall back to use JDK_BOOT_DIR which is set in make-adopt-build-farm.sh
+    javaHome=${JDK_BOOT_DIR}
   else
     echo "Unable to find a suitable JAVA_HOME to build the cyclonedx-lib"
     exit 2
