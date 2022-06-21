2cdb6fae480bd5ec82b467e108c9bb314758c3f5
Adam Brousseau <adam.brousseau88@gmail.com>
2022-06-21 14:25:43 -0400

    Update to openssl 1.1.1p
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -263,7 +263,7 @@ updateOpenj9Sources() {
   if [ "${BUILD_CONFIG[BUILD_VARIANT]}" == "${BUILD_VARIANT_OPENJ9}" ]; then
     cd "${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}" || return
     # NOTE: fetched openssl will NOT be used in the RISC-V cross-compile situation
-    bash get_source.sh --openssl-version=1.1.1o
+    bash get_source.sh --openssl-version=1.1.1p
     cd "${BUILD_CONFIG[WORKSPACE_DIR]}"
   fi
 }
