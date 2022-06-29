ad435e023b233bd67aef8578bc1bed6517799bae
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2022-06-29 15:47:41 +0100

    Update windows.sh

--- build-farm/platform-specific-configurations/windows.sh
+++ build-farm/platform-specific-configurations/windows.sh
@@ -139,9 +139,9 @@ then
       TOOLCHAIN_VERSION="2013"
       export BUILD_ARGS="${BUILD_ARGS} --freetype-version 2.5.3"
       export PATH="/cygdrive/c/openjdk/make-3.82/:$PATH"
-    elif [ "${JAVA_TO_BUILD}" == "${JDK11_VERSION}" ]
-    then
-      TOOLCHAIN_VERSION="2017"
+    elif [ "${JAVA_TO_BUILD}" == "${JDK8_VERSION}" ]
+    then 
+      export TOOLCHAIN_VERSION="2017"
       export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --disable-ccache"
     elif [ "$JAVA_FEATURE_VERSION" -gt 11 ]
     then
