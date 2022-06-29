67770c8dbea59ff1f87182fe0949279ef1edddf1
Wen Zhou <wenzhou@redhat.com>
2022-06-29 16:36:46 +0200

    feat: enable reproducible build for jdk17 (#3006)
    * feat: enable reproducible build for jdk17
    
    Fixes: https://github.com/adoptium/temurin-build/issues/2977
    
    * fix: jdk19+ or jdk17, exclude jdk18 for now
    
    * fix: linter
    
    * update: comments

--- build-farm/set-platform-specific-configurations.sh
+++ build-farm/set-platform-specific-configurations.sh
@@ -24,8 +24,8 @@ then
     export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --disable-warnings-as-errors"
 fi
 
-# jdk-19 and above support reproducible builds
-if [[ "${JAVA_FEATURE_VERSION}" -ge 19 ]]
+# jdk-17 and jdk-19+ support reproducible builds
+if [[ "${JAVA_FEATURE_VERSION}" -ge 19 || "${JAVA_FEATURE_VERSION}" -eq 17 ]]
 then
     # Enable reproducible builds implicitly with --with-source-date
     if [ "${RELEASE}" == "true" ]
