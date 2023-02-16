57dee4a64023f11bf48af294a3e6e7617272e81c
julian55455 <atuhwerajulian2@gmail.com>
2023-02-16 14:44:40 -0800

    fix lint

--- cyclonedx-lib/sign_src/TemurinSignSBOM.java
+++ cyclonedx-lib/sign_src/TemurinSignSBOM.java
@@ -1,17 +1,17 @@
 /**
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-################################################################################
-*/
+ * # Licensed under the Apache License, Version 2.0 (the "License");
+ * # you may not use this file except in compliance with the License.
+ * # You may obtain a copy of the License at
+ * #
+ * #      https://www.apache.org/licenses/LICENSE-2.0
+ * #
+ * # Unless required by applicable law or agreed to in writing, software
+ * # distributed under the License is distributed on an "AS IS" BASIS,
+ * # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * # See the License for the specific language governing permissions and
+ * # limitations under the License.
+ * ################################################################################
+ */
 package temurin.sbom;
 
 import org.cyclonedx.BomGeneratorFactory;
@@ -49,6 +49,7 @@ public final class TemurinSignSBOM {
 
     private TemurinSignSBOM() {
     }
+
     /**
      * Main entry.
      * @param args Arguments for sbom operation.
@@ -62,30 +63,30 @@ public final class TemurinSignSBOM {
         String fileName = null;
 
         for (int i = 0; i < args.length; i++) {
-          if (args[i].equals("--jsonFile")) {
-              fileName = args[++i];
-          } else if (args[i].equals("--privateKeyFile")) {
-              privateKeyFile = args[++i];
-          } else if (args[i].equals("--publicKeyFile")) {
-              publicKeyFile = args[++i];
-          } else if (args[i].equals("--signSBOM")) {
-              cmd = "signSBOM";
-              privateKeyFile = args[++i];
-          } else if (args[i].equals("--verifySignature")) {
-              cmd = "verifySignature";
-          } else if (args[i].equals("--verbose")) {
-              verbose = true;
-          }
-      }
+            if (args[i].equals("--jsonFile")) {
+                fileName = args[++i];
+            } else if (args[i].equals("--privateKeyFile")) {
+                privateKeyFile = args[++i];
+            } else if (args[i].equals("--publicKeyFile")) {
+                publicKeyFile = args[++i];
+            } else if (args[i].equals("--signSBOM")) {
+                cmd = "signSBOM";
+                privateKeyFile = args[++i];
+            } else if (args[i].equals("--verifySignature")) {
+                cmd = "verifySignature";
+            } else if (args[i].equals("--verbose")) {
+                verbose = true;
+            }
+        }
 
 
-      if (cmd.equals("signSBOM")) {
-          Bom bom = signSBOM(fileName, privateKeyFile);
-          writeJSONfile(bom, fileName);
-      } else if (cmd.equals("verifySignature")) {
-          boolean isValid = verifySignature(fileName, publicKeyFile);
-          System.out.println("Signature verification result: " + (isValid ? "Valid" : "Invalid"));
-      }
+        if (cmd.equals("signSBOM")) {
+            Bom bom = signSBOM(fileName, privateKeyFile);
+            writeJSONfile(bom, fileName);
+        } else if (cmd.equals("verifySignature")) {
+            boolean isValid = verifySignature(fileName, publicKeyFile);
+            System.out.println("Signature verification result: " + (isValid ? "Valid" : "Invalid"));
+        }
     }
 
     static Bom signSBOM(final String jsonFile, final String pemFile) {
@@ -122,64 +123,53 @@ public final class TemurinSignSBOM {
         return null;
     }
 
-
-  /*static Bom readJSONfile(final String jsonFile) throws IOException {
-      String jsonData = new String(Files.readAllBytes(Paths.get(jsonFile)), StandardCharsets.UTF_8);
-      JsonParser parser = new JsonParser();
-      return parser.parse(new StringReader(jsonData));
-  }
-
-  static void writeJSONfile(final Bom bom, final String jsonFile) throws IOException {
-      JSONObjectWriter writer = new JSONObjectWriter(bom.toJson());
-      Files.write(Paths.get(jsonFile), writer.serializeToBytes(JSONOutputFormats.PRETTY_PRINT));
-  }*/
-
-  static String generateBomJson(final Bom bom) {
-      BomJsonGenerator bomGen = BomGeneratorFactory.createJson(CycloneDxSchema.Version.VERSION_14, bom);
-      String json = bomGen.toJsonString();
-      return json;
-  }
-
-  static void writeJSONfile(final Bom bom, final String fileName) {          // Creates testJson.json file
-      FileWriter file;
-      String json = generateBomJson(bom);
-    try {
-      file = new FileWriter(fileName);
-      file.write(json);
-      file.close();
-  } catch (Exception e) {
-      e.printStackTrace();
+    static String generateBomJson(final Bom bom) {
+        BomJsonGenerator bomGen = BomGeneratorFactory.createJson(CycloneDxSchema.Version.VERSION_14, bom);
+        String json = bomGen.toJsonString();
+        return json;
     }
-  }
 
-  static Bom readJSONfile(final String fileName) { 	                               // Returns parse bom
-    Bom bom = null;
-    try {
-        FileReader reader = new FileReader(fileName);
-        JsonParser parser = new JsonParser();
-        bom = parser.parse(reader);
-    } catch (Exception e) {
-        e.printStackTrace();
-    } finally {
-       return bom;
+    static void writeJSONfile(final Bom bom, final String fileName) {
+        // Creates testJson.json file
+        FileWriter file;
+        String json = generateBomJson(bom);
+        try {
+            file = new FileWriter(fileName);
+            file.write(json);
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  static boolean verifySignature(final String jsonFile, final String publicKeyFile) {
-    try {
-      // Read the JSON file to be verified
-      Bom bom = readJSONfile(jsonFile);
-      String signedSbomData = generateBomJson(bom);
-
-      // Parse json
-      JSONObjectReader reader = JSONParser.parse(signedSbomData);
-
-      // Verify signature
-      JSONSignatureDecoder signature = reader.getSignature(new JSONCryptoHelper.Options());
-      signature.verify(new JSONAsymKeyVerifier(publicKey));
-    } catch (Exception e) {
-      e.printStackTrace();
+    static Bom readJSONfile(final String fileName) {                                 // Returns parse bom
+        Bom bom = null;
+        try {
+            FileReader reader = new FileReader(fileName);
+            JsonParser parser = new JsonParser();
+            bom = parser.parse(reader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return bom;
+        }
     }
-      return false;
-  }
+
+    static boolean verifySignature(final String jsonFile, final String publicKeyFile) {
+        try {
+            // Read the JSON file to be verified
+            Bom bom = readJSONfile(jsonFile);
+            String signedSbomData = generateBomJson(bom);
+
+            // Parse JSON
+            JSONObjectReader reader = JSONParser.parse(signedSbomData);
+
+            // Verify signature
+            JSONSignatureDecoder signature = reader.getSignature(new JSONCryptoHelper.Options());
+            signature.verify(new JSONAsymKeyVerifier(publicKey));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
