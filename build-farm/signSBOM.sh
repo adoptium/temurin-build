425478a3319fc06b69424685d2e4f4c96939e5e2
julian55455 <atuhwerajulian2@gmail.com>
2023-02-20 10:44:45 -0800

    fix linter warnings

--- build-farm/signSBOM.sh
+++ build-farm/signSBOM.sh
@@ -23,18 +23,15 @@ fi
 # Set the SCRIPT_DIR variable
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
-# Make the signSBOM.sh file executable
-chmod +x "${SCRIPT_DIR}/../build-farm/signSBOM.sh"
+# Add executable permission to signSBOM.sh and run it
+"${SCRIPT_DIR}"/../sbom.sh signSBOMFile
+chmod +x "${SCRIPT_DIR}"/../sbom.sh
+"${SCRIPT_DIR}"/../sbom.sh verifySBOMSignature
 
 # Run the ant build command to build the org.webpki.json openkeystore code and call the signSBOMFile() function in sbom.sh
-if ! ant -buildfile "${SCRIPT_DIR}/../cyclonedx-lib/build.xml" buildSignSBOM; then
+if ! ant -buildfile "${SCRIPT_DIR}"/../cyclonedx-lib/build.xml buildSignSBOM; then
   echo "Error: The ant build command to build the org.webpki.json openkeystore code failed."
   exit 1
 fi
-
-"${SCRIPT_DIR}/../sbom.sh" signSBOMFile
-
-# Call the verifySBOMSignature() function in sbom.sh
-"${SCRIPT_DIR}/../sbom.sh" verifySBOMSignature
 
 echo "Success: The org.webpki.json openkeystore code was built and signed successfully using JDK-17."
