6768f3757e6592046fc1b6d0e91b669d07885bf3
Scott Fryer <scfryer@redhat.com>
2023-05-23 16:34:11 +0100

    Restore alpine specific logic

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -295,18 +295,27 @@ checkingAndDownloadingAlsa() {
   if [[ -n "$FOUND_ALSA" ]]; then
     echo "Skipping ALSA download"
   else
+    
     ALSA_BUILD_URL="https://ftp.osuosl.org/pub/blfs/conglomeration/alsa-lib/alsa-lib-${ALSA_LIB_VERSION}.tar.bz2"
     curl -o "alsa-lib.tar.bz2" "$ALSA_BUILD_URL"
     curl -o "alsa-lib.tar.bz2.sig" "https://www.alsa-project.org/files/pub/lib/alsa-lib-${ALSA_LIB_VERSION}.tar.bz2.sig"
-    # WORKSPACE in preference as Alpine fails gpg operation if PWD > 83 characters
-    export GNUPGHOME="${WORKSPACE:-$PWD}/.gpg-temp"
-    # Should we clear this directory up after checking?
-    # Would this risk removing anyone's existing dir with that name?
-    # Erring on the side of caution for now
-    mkdir -p "$GNUPGHOME" && chmod og-rwx "$GNUPGHOME"
-    gpg --keyserver keyserver.ubuntu.com --recv-keys "${ALSA_LIB_GPGKEYID}" || true
-    echo -e "5\ny\n" |  gpg --batch --command-fd 0 --expert --edit-key "${ALSA_LIB_GPGKEYID}" trust; true
-    gpg --verify alsa-lib.tar.bz2.sig alsa-lib.tar.bz2 || exit 1
+
+    ## Add Exclusion For Alpine Linx As This Doesnt Work In docker
+
+    if echo ${BUILD_CONFIG[OS_KERNEL_NAME]} | grep -qi "alpine" ; then
+      downloadFile "alsa-lib.tar.bz2" "https://ftp.osuosl.org/pub/blfs/conglomeration/alsa-lib/alsa-lib-${ALSA_LIB_VERSION}.tar.bz2" "${ALSA_LIB_CHECKSUM}"
+      ALSA_BUILD_INFO="https://ftp.osuosl.org/pub/blfs/conglomeration/alsa-lib/alsa-lib-${ALSA_LIB_VERSION}.tar.bz2"
+    else
+      # WORKSPACE in preference as Alpine fails gpg operation if PWD > 83 characters
+      export GNUPGHOME="${WORKSPACE:-$PWD}/.gpg-temp"
+      # Should we clear this directory up after checking?
+      # Would this risk removing anyone's existing dir with that name?
+      # Erring on the side of caution for now
+      mkdir -p "$GNUPGHOME" && chmod og-rwx "$GNUPGHOME"
+      gpg --keyserver keyserver.ubuntu.com --recv-keys "${ALSA_LIB_GPGKEYID}"
+      echo -e "5\ny\n" |  gpg --batch --command-fd 0 --expert --edit-key "${ALSA_LIB_GPGKEYID}" trust;
+      gpg --verify alsa-lib.tar.bz2.sig alsa-lib.tar.bz2 || exit 1
+    fi
     if [[ "${BUILD_CONFIG[OS_KERNEL_NAME]}" == "aix" ]] || [[ "${BUILD_CONFIG[OS_KERNEL_NAME]}" == "sunos" ]]; then
       bzip2 -d alsa-lib.tar.bz2
       tar -xf alsa-lib.tar --strip-components=1 -C "${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/installedalsa/"
