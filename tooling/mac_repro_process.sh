955ce22c87cd935cc73e1ad91e880d5222492727
Andrew Leonard <anleonar@redhat.com>
2023-05-24 16:33:06 +0100

    Refactor common functions
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tooling/mac_repro_process.sh
+++ tooling/mac_repro_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2086
+# shellcheck disable=SC2086,SC1091
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
