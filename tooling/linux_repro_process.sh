d0c2cddfc411e51aedd452ae75c0931cc92c768f
Sophia Guo <sophia.gwf@gmail.com>
2023-06-22 14:01:50 -0400

    Turn off errexit
    Set OS=Darwin instead of using 'uname' as the compare job only runs on
    linux
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- tooling/linux_repro_process.sh
+++ tooling/linux_repro_process.sh
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-
 source repro_common.sh
-
-set -e
 
 JDK_DIR="$1"
 
--- tooling/mac_repro_process.sh
+++ tooling/mac_repro_process.sh
@@ -29,7 +29,7 @@ if [[ ! -d "${JDK_DIR}" ]] || [[ ! -d "${JDK_DIR}/bin"  ]]; then
   exit 1
 fi
 
-OS=$("uname")
+OS="Darwin"
 
 expandJDK "$JDK_DIR"
 
