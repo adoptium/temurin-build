b4400d0087dba74c8459a1984f1d9b1831b72aba
Andrew Leonard <anleonar@redhat.com>
2023-08-04 14:35:19 +0100

    Fix SBOM glibc and gcc version info
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- sbin/build.sh
+++ sbin/build.sh
@@ -928,7 +928,9 @@ cat /home/jenkins/workspace/build-scripts/jobs/jdk8u/jdk8u-linux-x64-temurin/wor
      local CC=$(grep "^CC :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk)
      # Remove env=xx from CC, so we can call from bash to get __GLIBC.
      CC=$(echo "$CC" | tr -s " " | cut -d" " -f3- | sed -E "s/[^ ]*=[^ ]*//g")
+echo "$CC"
      local SYSROOT_CFLAGS=$(grep "^SYSROOT_CFLAGS :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk | tr -s " " | cut -d" " -f3-)
+echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1
      local GLIBC_MAJOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC__" | cut -d" " -f3)
      local GLIBC_MINOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC_MINOR__" | cut -d" " -f3)
      local GLIBC_VERSION="${GLIBC_MAJOR}.${GLIBC_MINOR}"
