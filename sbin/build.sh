bc5e8ec2631af15c469bb59b15244f127d8fe906
Andrew Leonard <anleonar@redhat.com>
2023-08-04 14:10:13 +0100

    Fix SBOM glibc and gcc version info
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- sbin/build.sh
+++ sbin/build.sh
@@ -916,24 +916,26 @@ checkingToolSummary() {
 # Below add versions to sbom | Facilitate reproducible builds
 
 addGLIBCforLinux() {
-   # Get GLIBC from configured build spec.gmk sysroot and features.h definitions
+   # Alpine uses "musl" rather than "glibc"
+   if [[ "${BUILD_CONFIG[OS_FULL_VERSION]}" == *"Alpine"* ]]; then
+     local MUSL_VERSION=$("/lib/libc.musl-x86_64.so.1" 2>&1 | grep "Version" | tr -s " " | cut -d" " -f2)
+     echo "Adding MUSL version to SBOM: ${MUSL_VERSION}"
+     addSBOMMetadataTools "${javaHome}" "${classpath}" "${sbomJson}" "MUSL" "${MUSL_VERSION}"
+   else
+     # Get GLIBC from configured build spec.gmk sysroot and features.h definitions
 
-   # Get CC and SYSROOT_CFLAGS from the built build spec.gmk.
-   local CC=$(grep "^CC :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk)
-   # Remove env=xx from CC, so we can call from bash to get __GLIBC.
-echo $CC
-   CC=$(echo "$CC" | tr -s " " | cut -d" " -f3- | sed -E "s/[^ ]*=[^ ]*//g")
-echo $CC
-   local SYSROOT_CFLAGS=$(grep "^SYSROOT_CFLAGS :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk | tr -s " " | cut -d" " -f3-)
-echo $SYSROOT_CFLAGS
-# echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1
-"/lib/libc.musl-x86_64.so.1"
-   local GLIBC_MAJOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC__" | cut -d" " -f3)
-   local GLIBC_MINOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC_MINOR__" | cut -d" " -f3)
-   local GLIBC_VERSION="${GLIBC_MAJOR}.${GLIBC_MINOR}"
+     # Get CC and SYSROOT_CFLAGS from the built build spec.gmk.
+     local CC=$(grep "^CC :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk)
+     # Remove env=xx from CC, so we can call from bash to get __GLIBC.
+     CC=$(echo "$CC" | tr -s " " | cut -d" " -f3- | sed -E "s/[^ ]*=[^ ]*//g")
+     local SYSROOT_CFLAGS=$(grep "^SYSROOT_CFLAGS :=" ${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}/${BUILD_CONFIG[OPENJDK_SOURCE_DIR]}/build/*/spec.gmk | tr -s " " | cut -d" " -f3-)
+     local GLIBC_MAJOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC__" | cut -d" " -f3)
+     local GLIBC_MINOR=$(echo "#include <features.h>" | $CC $SYSROOT_CFLAGS -dM -E - 2>&1 | tr -s " " | grep "#define __GLIBC_MINOR__" | cut -d" " -f3)
+     local GLIBC_VERSION="${GLIBC_MAJOR}.${GLIBC_MINOR}"
 
-   echo "Adding GLIBC version to SBOM: ${GLIBC_VERSION}"
-   addSBOMMetadataTools "${javaHome}" "${classpath}" "${sbomJson}" "GLIBC" "${GLIBC_VERSION}"
+     echo "Adding GLIBC version to SBOM: ${GLIBC_VERSION}"
+     addSBOMMetadataTools "${javaHome}" "${classpath}" "${sbomJson}" "GLIBC" "${GLIBC_VERSION}"
+   fi
 }
 
 addGCC() {
