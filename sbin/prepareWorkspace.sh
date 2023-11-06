514b577751327fca730ad831d362047c2a1100f4
Haroon Khel <hkhel@redhat.com>
2023-11-06 18:13:56 +0000

    debug

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -491,6 +491,7 @@ checkingAndDownloadingFreeType() {
 
     # We get the files we need at $WORKING_DIR/installedfreetype
     # shellcheck disable=SC2046
+    echo "pngArg value: ${pngArg}"
     if ! (bash ./configure --prefix="${BUILD_CONFIG[WORKSPACE_DIR]}/${BUILD_CONFIG[WORKING_DIR]}"/installedfreetype "${pngArg}" "${BUILD_CONFIG[FREETYPE_FONT_BUILD_TYPE_PARAM]}" && ${BUILD_CONFIG[MAKE_COMMAND_NAME]} all && ${BUILD_CONFIG[MAKE_COMMAND_NAME]} install); then
       # shellcheck disable=SC2154
       echo "Failed to configure and build libfreetype, exiting"
