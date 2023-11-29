554ad74bbeccee28e1719fbfb6d64ea13198f587
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2023-11-29 11:10:53 +0000

    Tidy Ubuntu check logic

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -197,17 +197,11 @@ then
   ## Fix For Issue https://github.com/adoptium/temurin-build/issues/3547
   ## Add Missing Library Path For Ubuntu 22+
   if [ -e /etc/os-release ]; then
-    ## source the os -release file if present
-    . /etc/os-release
-    INT_VERSION_ID=$(echo "$VERSION_ID" | awk -F'.' '{print $1'})
+    ID=$(cat /etc/os-release | grep ^ID= | awk -F'=' '{print $2'})
+    INT_VERSION_ID=$(cat /etc/os-release | grep ^VERSION_ID= | awk -F'"' '{print $2'} | awk -F'.' '{print $1}')
     if [ "$ID" == "ubuntu" ] && [ "$INT_VERSION_ID" -ge "22" ]; then
-      echo "Ubuntu 22+"
       export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
-    else
-      echo "Not Ubuntu 22+"
     fi
-  else
-    echo "Unable To Determine The O/S"
   fi
 fi
 
