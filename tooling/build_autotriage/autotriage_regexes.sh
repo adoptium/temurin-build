21af64b75fd7c311f839da6f06ae14bf30dc4c49
Adam Farley <adfarley@redhat.com>
2023-11-29 19:28:48 +0000

    Creating an automated build triage script (#3549)
    This will create a weekly issue that contains a list of the latest
    failing Temurin builds at Adoptium.
    
    Signed-off-by: Adam Farley <adfarley@redhat.com>

---
+++ tooling/build_autotriage/autotriage_regexes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+################################################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+################################################################################
+#
+# This script produces arrays of regular expressions that match a type of failure
+# that can be searched for in the output of an Eclipse Temurin build of OpenJDK.
+# 
+# Each regular expression comes paired with metadata (providing useful information)
+#
+################################################################################
+
+# declare -a arrayOfRegexes
+# declare -a arrayOfRegexMetadata
+# declare -a arrayOfRegexPreventability
+
+# TODO.
