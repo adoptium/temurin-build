67bb1b591a00e8c23aa28a84552918660436d3e1
henning410 <henning1.weise@web.de>
2024-01-12 23:52:53 +0100

    refactored strace_analysis script

--- tooling/strace_analysis.sh
+++ tooling/strace_analysis.sh
@@ -6,71 +6,21 @@
 # $4 is classpath
 # $5 is sbomJson
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/../sbin/common/sbom.sh"
-
-if [ -z "$1" ]; then
-    echo "strace output folder as param is missing!"
-    exit 1
-fi
-
-if [ -z "$2" ]; then
-    echo "temurin-build folder as param is missing!"
-    exit 1
-fi
-
-javaHome=$3
-classpath=$4
-sbomJson=$5
-
-
-isBinSymLink=false
-isLibSymLink=false
-isSbinSymLink=false
-
-# Check if /bin, /lib, /sbin are symlinks, as sometimes pkgs are installed
-# under the symlink folder, eg.in Ubuntu 20.04
-binDir=$(readlink -f "/bin")
-if [[ "$binDir" != "/bin" ]]; then
-    isBinSymLink=true
-fi
-libDir=$(readlink -f "/lib")
-if [[ "$libDir" != "/lib" ]]; then
-    isLibSymLink=true
-fi
-sbinDir=$(readlink -f "/sbin")
-if [[ "$sbinDir" != "/sbin" ]]; then
-    isSbinSymLink=true
-fi
-
-echo "/bin is symlink: $isBinSymLink"
-echo "/lib is symlink: $isLibSymLink"
-echo "/sbin is symlink: $isSbinSymLink"
-echo ""
-
-echo "Param 1: $1"
-echo "Param 2: $2"
-echo "Param 3: $javaHome"
-echo "Param 4: $classpath"
-echo "Param 5: $sbomJson"
-
-# configure SBOM
-addSBOMFormulation "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies"
-addSBOMFormulationComp "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Strace derived package dependencies"
-addSBOMFormulationComp "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Strace derived non-package dependencies"
-
+declare -g javaHome
+declare -g classpath
+declare -g sbomJson
 
 # Arrays to store different types of strace output, to treat them different
-allFiles=()
-usrLocalFiles=()
-otherFiles=()
+declare -g usrLocalFiles=()
+declare -g otherFiles=()
 
 # Arrays for package and non-package dependencies
-pkgs=()
-npkgs=()
+declare -g pkgs=()
+declare -g npkgs=()
+declare -g uniqueVersions=()
 
-# grep strace files, ignoring:
-ignores=(
+# ignore-patterns for strace files
+declare -g ignores=(
   "\.java$"
   "\.d$"
   "\.o$"
@@ -84,118 +34,207 @@ ignores=(
   "^/tmp/"
 )
 
-grep_command="grep -Ev '(${ignores[0]}"
-for ((i=1; i<${#ignores[@]}; i++)); do
-    grep_command+="|${ignores[i]}"
-done
-grep_command+=")'"
+checkArguments() {
+  if [ -z "$1" ]; then
+      echo "strace output folder as param is missing!"
+      exit 1
+  fi
 
-# filtering out relevant parts of strace output files
-allFiles=($(find "$1" -type f -name 'outputFile.*'| xargs -n100 grep -v ENOENT | cut -d'"' -f2 | grep "^/" | eval $grep_command | sort | uniq))
+  if [ -z "$2" ]; then
+      echo "temurin-build folder as param is missing!"
+      exit 1
+  fi
 
-# go over all filtered files and store those with /usr/local in separate array
-for file in "${allFiles[@]}"; do
-    if [[ $file == "/usr/local/"* ]]; then
-        usrLocalFiles+=("$file")
-        echo "UsrLocalFile: $file"
-    else
-        otherFiles+=("$file")
-        echo "No usrLocalFile: $file"
-    fi
-done
+  if [ -z "$3" ]; then
+        echo "javaHome as param is missing!"
+        exit 1
+  fi
 
-echo "Length of /usr/local: ${#usrLocalFiles[@]}"
-echo "Length of other files: ${#otherFiles[@]}"
+  if [ -z "$4" ]; then
+          echo "classpath as param is missing!"
+          exit 1
+  fi
 
-for file in "${otherFiles[@]}"; do
-    echo "Processing: $file"
+  if [ -z "$5" ]; then
+          echo "sbomJson as param is missing!"
+          exit 1
+  fi
 
-    filePath="$(readlink -f "$file")"
-    pkg=$(rpm -qf "$filePath")
+  javaHome=$3
+  classpath=$4
+  sbomJson=$5
 
-    rc=$?
-    if [[ "$rc" != "0" ]]; then
-        # bin, lib, sbin pkgs may be installed under the root symlink
-        if [[ "$isBinSymLink" == "true" ]] && [[ $filePath == /usr/bin* ]]; then
-            filePath=${filePath/#\/usr\/bin/}
-            filePath="/bin${filePath}"
-            pkg=$(rpm -qf "$filePath" 2>/dev/null)
-            rc=$?
-        fi
-        if [[ "$isLibSymLink" == "true" ]] && [[ $filePath == /usr/lib* ]]; then
-            filePath=${filePath/#\/usr\/lib/}
-            filePath="/lib${filePath}"
-            pkg=$(rpm -qf "$filePath" 2>/dev/null)
-            rc=$?
-        fi
-        if [[ "$isSbinSymLink" == "true" ]] && [[ $filePath == /usr/sbin* ]]; then
-            filePath=${filePath/#\/usr\/sbin/}
-            filePath="/sbin${filePath}"
-            pkg=$(rpm -qf "$filePath" 2>/dev/null)
-            rc=$?
-        fi
-    fi
+  echo "Strace output folder: $1"
+  echo "Temurin build folder: $2"
+  echo "javaHome: $javaHome"
+  echo "classpath: $classpath"
+  echo "sbomJson: $sbomJson"
+}
 
-    ignoreFile=false
-    for ignoreFile in "${ignores[@]}"; do
-        if [[ "$filePath" =~ $ignoreFile ]]; then
-            ignoreFile=true
-            break
+checkSymLinks() {
+  isBinSymLink=false
+  isLibSymLink=false
+  isSbinSymLink=false
+
+  # Check if /bin, /lib, /sbin are symlinks, as sometimes pkgs are installed
+  # under the symlink folder, eg.in Ubuntu 20.04
+  binDir=$(readlink -f "/bin")
+  if [[ "$binDir" != "/bin" ]]; then
+      isBinSymLink=true
+  fi
+  libDir=$(readlink -f "/lib")
+  if [[ "$libDir" != "/lib" ]]; then
+      isLibSymLink=true
+  fi
+  sbinDir=$(readlink -f "/sbin")
+  if [[ "$sbinDir" != "/sbin" ]]; then
+      isSbinSymLink=true
+  fi
+
+  echo "/bin is symlink: $isBinSymLink"
+  echo "/lib is symlink: $isLibSymLink"
+  echo "/sbin is symlink: $isSbinSymLink"
+  echo ""
+}
+
+configureSbom() {
+  addSBOMFormulation "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies"
+  addSBOMFormulationComp "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Build tool package dependencies"
+  addSBOMFormulationComp "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Build tool non-package dependencies"
+}
+
+filterStraceFiles() {
+  allFiles=()
+  # Configure grep command to use ignore-patterns
+  grep_command="grep -Ev '(${ignores[0]}"
+  for ((i=1; i<${#ignores[@]}; i++)); do
+      grep_command+="|${ignores[i]}"
+  done
+  grep_command+=")'"
+
+  # filtering out relevant parts of strace output files
+  allFiles=($(find "$1" -type f -name 'outputFile.*'| xargs -n100 grep -v ENOENT | cut -d'"' -f2 | grep "^/" | eval $grep_command | sort | uniq))
+
+  # loop over all filtered files and store those with /usr/local in separate array
+  for file in "${allFiles[@]}"; do
+      if [[ $file == "/usr/local/"* ]]; then
+          usrLocalFiles+=("$file")
+          echo "UsrLocalFile: $file"
+      else
+          otherFiles+=("$file")
+          echo "No usrLocalFile: $file"
+      fi
+  done
+
+  echo "Number of /usr/local files: ${#usrLocalFiles[@]}"
+  echo "Number of other files: ${#otherFiles[@]}"
+}
+
+printNumberOfAllProcessedFiles() {
+  # Calculate and print number of all processed strace output files
+  totalLength=$(( ${#usrLocalFiles[@]} + ${#otherFiles[@]} ))
+  if [ $totalLength -ne 0 ]; then
+      printf '\nNumber of all processed strace output files: %s\n' "$totalLength"
+  else
+      printf "\nNo strace output files available\n"
+      exit 1
+  fi
+}
+
+processFiles() {
+  for file in "${otherFiles[@]}"; do
+      echo "Processing: $file"
+
+      filePath="$(readlink -f "$file")"
+      pkg=$(rpm -qf "$filePath")
+
+      rc=$?
+      if [[ "$rc" != "0" ]]; then
+          # bin, lib, sbin pkgs may be installed under the root symlink
+          if [[ "$isBinSymLink" == "true" ]] && [[ $filePath == /usr/bin* ]]; then
+              filePath=${filePath/#\/usr\/bin/}
+              filePath="/bin${filePath}"
+              pkg=$(rpm -qf "$filePath" 2>/dev/null)
+              rc=$?
+          fi
+          if [[ "$isLibSymLink" == "true" ]] && [[ $filePath == /usr/lib* ]]; then
+              filePath=${filePath/#\/usr\/lib/}
+              filePath="/lib${filePath}"
+              pkg=$(rpm -qf "$filePath" 2>/dev/null)
+              rc=$?
+          fi
+          if [[ "$isSbinSymLink" == "true" ]] && [[ $filePath == /usr/sbin* ]]; then
+              filePath=${filePath/#\/usr\/sbin/}
+              filePath="/sbin${filePath}"
+              pkg=$(rpm -qf "$filePath" 2>/dev/null)
+              rc=$?
+          fi
+      fi
+
+      ignoreFile=false
+      for ignoreFile in "${ignores[@]}"; do
+          if [[ "$filePath" =~ $ignoreFile ]]; then
+              ignoreFile=true
+              break
+          fi
+      done
+      if [[ $ignoreFile == true ]]; then
+          continue
+      fi
+
+      if [[ "$rc" != "0" ]]; then
+          #echo "no pkg: $filePath"
+          usrLocalFiles+=("$filePath")
+      else
+          pkg="$(echo "$pkg" | cut -d" " -f1)"
+          pkg=${pkg::-1}
+          #echo "file: $filePath pkg: $pkg version: $pkg"
+          pkgString="pkg: $pkg version: $pkg"
+          if ! echo "${pkgs[@]-}" | grep "temurin_${pkgString}_temurin" >/dev/null; then
+              addSBOMFormulationComponentProperty "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Build tool package dependencies" "${pkg}" "${pkg}"
+              pkgs+=("temurin_${pkgString}_temurin")
+          fi
+      fi
+  done
+}
+
+processUsrLocalFiles() {
+    # loop over all non-package dependencies and try to get the version. If version is not empty, add to array
+    for file in "${usrLocalFiles[@]-}"; do
+        npkg=$("$file" --version 2>/dev/null | head -n 1)
+
+        if [[ "$npkg" != "" ]]; then
+            version=$(echo "$npkg" | awk '{print $NF}')
+            if [[ ! " ${uniqueVersions[@]-} " =~ " ${npkg} " ]]; then
+                npkgs+=("${npkg}")
+                uniqueVersions+=("${npkg}")   # Marking package as processed
+                
+                addSBOMFormulationComponentProperty "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Build tool non-package dependencies" "${npkg}" "${version}"
+            fi
         fi
     done
-    if [[ $ignoreFile == true ]]; then
-        continue
-    fi
+}
 
-    if [[ "$rc" != "0" ]]; then
-        #echo "no pkg: $filePath"
-        usrLocalFiles+=("$filePath")
-    else
-        pkg="$(echo "$pkg" | cut -d" " -f1)"
-        pkg=${pkg::-1}
-        #echo "file: $filePath pkg: $pkg version: $pkg"
-        pkgString="pkg: $pkg version: $pkg"
-        addSBOMFormulationComponentProperty "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Strace derived package dependencies" "${pkg}" "${pkg}"
-        if ! echo "${pkgs[@]}" | grep "temurin_${pkgString}_temurin" >/dev/null; then
-            pkgs+=("temurin_${pkgString}_temurin")
-        fi
-    fi
-done
+printPackages() {
+  echo -e "\nNon-Package Dependencies:"
+  printf '%s\n' "${npkgs[@]-}" | sort -u
 
-# Calculate and print number of all processed strace output files
-totalLength=$(( ${#usrLocalFiles[@]} + ${#otherFiles[@]} ))
-if [ $totalLength -ne 0 ]; then
-    printf '\nNumber of all processed strace output files: %s\n' "$totalLength"
-else
-    printf "\nNo strace output files available\n"
-    exit 1
-fi
+  echo -e "\nPackage Dependencies:"
+  for pkg in "${pkgs[@]}"; do
+      trimPkg=${pkg/#temurin_/}
+      trimPkg=${trimPkg%_temurin}
+      echo $trimPkg
+  done
+  echo -e "\n"
+}
 
-# loop over all non-package dependencies and try to get the version. If version is not empty, add to array
-for file in "${usrLocalFiles[@]-}"; do
-    printf 'File to check version %s' "$file"
-    npkg=$("$file" --version 2>/dev/null | head -n 1)
-    if [[ "$npkg" != "" ]]; then
-        npkgs+=("${npkg}")
-        # TODO: Cut version out
-        version=$(echo "$npkg" | awk '{print $NF}')
-        addSBOMFormulationComponentProperty "${javaHome}" "${classpath}" "${sbomJson}" "Build Dependencies" "Strace derived non-package dependencies" "${npkg}" "${version}"
-    fi
-done
-
-echo -e "\nNon-Package Dependencies:"
-printf '%s\n' "${npkgs[@]-}" | sort -u
-
-echo -e "\nPackage Dependencies:"
-for pkg in "${pkgs[@]}"; do
-    trimPkg=${pkg/#temurin_/}
-    trimPkg=${trimPkg%_temurin}
-    echo $trimPkg
-done
-
-# addSBOMFormulationComp "${javaHome}" "${classpath}" "${sbomJson}" "CycloneDX" "CycloneDX jar SHAs"
-
-# "${javaHome}"/bin/java -cp "${classpath}" temurin.sbom.TemurinGenSBOM --addFormulationComp --jsonFile "${jsonFile}" --formulaName "${formulaName}" --name "${name}"
-
-
-echo -e "\n"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../sbin/common/sbom.sh"
+checkArguments "$@"
+checkSymLinks
+configureSbom
+filterStraceFiles "$@"
+processFiles
+processUsrLocalFiles
+printNumberOfAllProcessedFiles
+printPackages
