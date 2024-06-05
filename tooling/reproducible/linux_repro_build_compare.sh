b2050b542b442f4b8ed8484714611d468d65bb2e
Sophia Guo <sophia.gwf@gmail.com>
2024-06-05 11:31:18 -0400

    Update build args setting
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- tooling/reproducible/linux_repro_build_compare.sh
+++ tooling/reproducible/linux_repro_build_compare.sh
@@ -25,7 +25,7 @@ ANT_SHA=9028e2fc64491cca0f991acc09b06ee7fe644afe41d1d6caf72702ca25c4613c
 ANT_CONTRIB_VERSION=1.0b3
 ANT_CONTRIB_SHA=4d93e07ae6479049bb28071b069b7107322adaee5b70016674a0bffd4aac47f9
 isJdkDir=false
-
+USING_DEVKIT="true"
 installPrereqs() {
   if test -r /etc/redhat-release; then
     yum install -y gcc gcc-c++ make autoconf unzip zip alsa-lib-devel cups-devel libXtst-devel libXt-devel libXrender-devel libXrandr-devel libXi-devel
@@ -75,12 +75,15 @@ downloadAnt() {
   fi
 }
 
-setEnvironment() {
+setGccEnvironment() {
   export CC="${LOCALGCCDIR}/bin/gcc-${GCCVERSION}"
   export CXX="${LOCALGCCDIR}/bin/g++-${GCCVERSION}"
   export LD_LIBRARY_PATH="${LOCALGCCDIR}/lib64"
-  # /usr/local/bin required to pick up the new autoconf if required
+}
+
+setAntEnvironment() {
   export PATH="${LOCALGCCDIR}/bin:/usr/local/bin:/usr/bin:$PATH:/usr/local/apache-ant-${ANT_VERSION}/bin"
+  # /usr/local/bin required to pick up the new autoconf if required
   ls -ld "$CC" "$CXX" "/usr/lib/jvm/jdk-${BOOTJDK_VERSION}/bin/javac" || exit 1
 }
 
@@ -90,12 +93,29 @@ cleanBuildInfo() {
   sed -i '/^BUILD_INFO=.*$/d' "${DIR}/release"
 }
 
+setTemurinBuildArgs() {
+  local buildArgs="$1"
+  local bootJdk="$2"
+  local ignoreOptions=("--enable-sbom-strace ")
+  for ignoreOption in "${ignoreOptions[@]}"; do
+    buildArgs="${buildArgs/${ignoreOption}//}"
+  done
+  #reset --jdk-boot-dir
+  # shellcheck disable=SC2001
+  buildArgs="$(echo "$buildArgs" | sed -e "s|--jdk-boot-dir [^ ]*|--jdk-boot-dir /usr/lib/jvm/jdk-${bootJdk}|")"
+  echo "${buildArgs}"
+}
+
 downloadTooling() {
+  local using_DEVKIT=$1
+
   if [ ! -r "/usr/lib/jvm/jdk-${BOOTJDK_VERSION}/bin/javac" ]; then
     echo "Retrieving boot JDK $BOOTJDK_VERSION" && mkdir -p /usr/lib/jvm && curl -L "https://api.adoptium.net/v3/binary/version/jdk-${BOOTJDK_VERSION}/linux/${NATIVE_API_ARCH}/jdk/hotspot/normal/eclipse?project=jdk" | (cd /usr/lib/jvm && tar xpzf -)
   fi
-  if [ ! -r "${LOCALGCCDIR}/bin/g++-${GCCVERSION}" ]; then
-    echo "Retrieving gcc $GCCVERSION" && curl "https://ci.adoptium.net/userContent/gcc/gcc$(echo "$GCCVERSION" | tr -d .).$(uname -m).tar.xz" | (cd /usr/local && tar xJpf -) || exit 1
+  if [[ "${using_DEVKIT}" == "false" ]]; then
+    if [ ! -r "${LOCALGCCDIR}/bin/g++-${GCCVERSION}" ]; then
+      echo "Retrieving gcc $GCCVERSION" && curl "https://ci.adoptium.net/userContent/gcc/gcc$(echo "$GCCVERSION" | tr -d .).$(uname -m).tar.xz" | (cd /usr/local && tar xJpf -) || exit 1
+    fi
   fi
   if [ ! -r temurin-build ]; then
     git clone https://github.com/adoptium/temurin-build || exit 1
@@ -125,16 +145,24 @@ GCCVERSION=$(jq -r '.metadata.tools[] | select(.name == "GCC") | .version' "$SBO
 LOCALGCCDIR=/usr/local/gcc$(echo "$GCCVERSION" | cut -d. -f1)
 TEMURIN_BUILD_SHA=$(jq -r '.components[0] | .properties[] | select (.name == "Temurin Build Ref") | .value' "$SBOM" | awk -F/ '{print $NF}')
 TEMURIN_BUILD_ARGS=$(jq -r '.components[0] | .properties[] | select (.name == "makejdk_any_platform_args") | .value' "$SBOM")
-TEMURIN_BUILD_ARGS="${TEMURIN_BUILD_ARGS//--enable-sbom-strace /}"
 TEMURIN_VERSION=$(jq -r '.metadata.component.version' "$SBOM" | sed 's/-beta//' | cut -f1 -d"-")
 #BUILDSTAMP=$(jq -r '.components[0].properties[] | select(.name == "Build Timestamp") | .value' "$SBOM")
 NATIVE_API_ARCH=$(uname -m)
 if [ "${NATIVE_API_ARCH}" = "x86_64" ]; then NATIVE_API_ARCH=x64; fi
 if [ "${NATIVE_API_ARCH}" = "armv7l" ]; then NATIVE_API_ARCH=arm; fi
+if [[ "$TEMURIN_BUILD_ARGS" =~ .*"--use-adoptium-devkit".* ]]; then
+  USING_DEVKIT="false"
+fi
 
 checkAllVariablesSet
-downloadTooling
-setEnvironment
+downloadTooling "$USING_DEVKIT"
+if [[ "${USING_DEVKIT}" == "false" ]]; then
+  setGccEnvironment
+fi
+setAntEnvironment
+echo "original temurin build args is ${TEMURIN_BUILD_ARGS}"
+TEMURIN_BUILD_ARGS=$(setTemurinBuildArgs "$TEMURIN_BUILD_ARGS" "$BOOTJDK_VERSION")
+
 
 if [ -z "$JDK_PARAM" ] && [ ! -d "jdk-${TEMURIN_VERSION}" ] ; then
     JDK_PARAM="https://api.adoptium.net/v3/binary/version/jdk-${TEMURIN_VERSION}/linux/${NATIVE_API_ARCH}/jdk/hotspot/normal/eclipse?project=jdk"
@@ -155,6 +183,7 @@ if [ "${isJdkDir}" = true ]; then
   comparedDir=$JDK_PARAM
 fi
 
+echo "Rebuild Temurin build args is $TEMURIN_BUILD_ARGS"
 echo " cd temurin-build && ./makejdk-any-platform.sh $TEMURIN_BUILD_ARGS 2>&1 | tee build.$$.log" | sh
 
 echo Comparing ...
