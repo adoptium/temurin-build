25f016c4d9832d9083d678a40d3dcccab39365e5
Andrew Leonard <anleonar@redhat.com>
2024-06-14 16:03:23 +0100

    Enable building jdk23+ jdk(head) version branches
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- configureBuild.sh
+++ configureBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC2155
 # ********************************************************************************
 # Copyright (c) 2018 Contributors to the Eclipse Foundation
 #
