d857468faf16f4e0db6197fa2ca06fd576fd18b8
Andrew Leonard <anleonar@redhat.com>
2024-10-10 15:35:41 +0100

    Upgrade to CycloneDX 1.0.6 spec
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- cyclonedx-lib/sign_src/TemurinSignSBOM.java
+++ cyclonedx-lib/sign_src/TemurinSignSBOM.java
@@ -1,6 +1,6 @@
 /*
  * ********************************************************************************
- * Copyright (c) 2023,2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023, 2024 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) with this work for additional
  * information regarding copyright ownership.
