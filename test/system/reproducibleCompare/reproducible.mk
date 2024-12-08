10376add5bad6bbddfe11463c79861da03a30687
sophia-guo <sophia.gwf@gmail.com>
2024-12-07 19:22:43 -0500

    Enhance SBOM and JDK file check (#4078)
    * update params check
    
    * check if default sbom and jdk available
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>
    
    * check if default sbom and jdk available
    
    * Format
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>
    
    * update ERROR to info to avoid exit the test early
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>
    
    * update usage message
    
    ---------
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- test/system/reproducibleCompare/reproducible.mk
+++ test/system/reproducibleCompare/reproducible.mk
@@ -12,18 +12,29 @@
 # ********************************************************************************
 
 ifndef SBOM_FILE
-	SBOM_FILE := $(shell ls $(TEST_ROOT)/../jdkbinary/ | grep "sbom" | grep -v "metadata")
-	SBOM_FILE := $(TEST_ROOT)/../jdkbinary/$(SBOM_FILE)
+    SBOM_FILE := $(shell ls $(TEST_ROOT)/../jdkbinary/ | grep "sbom" | grep -v "metadata")
+    ifeq ($(strip $(SBOM_FILE)),)
+        $(info ERROR! NO SBOM_FILE AVAILABLE)
+        SBOM_FILE :=
+    else
+        SBOM_FILE := $(TEST_ROOT)/../jdkbinary/$(SBOM_FILE)
+    endif
 endif
+
 ifndef JDK_FILE
-	JDK_FILE := $(shell find $(TEST_ROOT)/../jdkbinary/ -type f -name '*-jdk_*.tar.gz')
-	ifneq (,$(findstring win,$(SPEC)))
-		JDK_FILE := $(shell find $(TEST_ROOT)/../jdkbinary/ -type f -name '*-jdk_*.zip')
-	endif
+    JDK_FILE := $(shell find $(TEST_ROOT)/../jdkbinary/ -type f -name '*-jdk_*.tar.gz')
+    ifneq (,$(findstring win,$(SPEC)))
+        JDK_FILE := $(shell find $(TEST_ROOT)/../jdkbinary/ -type f -name '*-jdk_*.zip')
+    endif
+    ifeq ($(strip $(JDK_FILE)),)
+        $(info ERROR! NO JDK_FILE AVAILABLE)
+    endif
 endif
 
 ifneq (,$(findstring linux,$(SPEC)))
-	SBOM_FILE := $(subst $(TEST_ROOT)/../jdkbinary,/home/jenkins/test,$(SBOM_FILE))
+    ifneq ($(strip $(SBOM_FILE)),)
+        SBOM_FILE := $(subst $(TEST_ROOT)/../jdkbinary,/home/jenkins/test,$(SBOM_FILE))
+    endif
 endif
 
 RM_DEBUGINFO := $(shell find $(TEST_JDK_HOME) -type f -name "*.debuginfo" -delete)
