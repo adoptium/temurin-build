a05f16524fc4cb642d17f1b998add9f1ab9463ac
Jiri Vanek <jvanek@redhat.com>
2025-03-03 13:41:03 +0100

    Moved license headers to  2025

--- tooling/compare-builds-postfunctions.sh
+++ tooling/compare-builds-postfunctions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ********************************************************************************
-# Copyright (c) 2024 Contributors to the Eclipse Foundation
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
 #
 # See the NOTICE file(s) with this work for additional
 # information regarding copyright ownership.
