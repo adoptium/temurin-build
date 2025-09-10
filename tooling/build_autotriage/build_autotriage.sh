40c46ef67adb8c0e83d54edb75031fa40aade27c
Adam Farley <adfarley@ibm.com>
2025-09-10 14:47:30 +0100

    Autotriage updates and fixes (#4229)
    Compensate for missing job status when upstream data source is incomplete
    Start triaging jdk26
    Add JDK17 triage for Linux/riscv64
    Add Solaris simplepipes to the list of jdk8 pipelines
    
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- tooling/build_autotriage/build_autotriage.sh
+++ tooling/build_autotriage/build_autotriage.sh
@@ -52,13 +52,11 @@ temurinPlatforms+=("alpine-linux-x64");     platformStart+=(8);  platformEnd+=(9
 temurinPlatforms+=("linux-aarch64");        platformStart+=(8);  platformEnd+=(99)
 temurinPlatforms+=("linux-arm");            platformStart+=(8);  platformEnd+=(20)
 temurinPlatforms+=("linux-ppc64le");        platformStart+=(8);  platformEnd+=(99)
-temurinPlatforms+=("linux-riscv64");        platformStart+=(21); platformEnd+=(99)
+temurinPlatforms+=("linux-riscv64");        platformStart+=(17); platformEnd+=(99)
 temurinPlatforms+=("linux-s390x");          platformStart+=(11); platformEnd+=(99)
 temurinPlatforms+=("linux-x64");            platformStart+=(8);  platformEnd+=(99)
 temurinPlatforms+=("mac-aarch64");          platformStart+=(11); platformEnd+=(99)
 temurinPlatforms+=("mac-x64");              platformStart+=(8);  platformEnd+=(99)
-temurinPlatforms+=("solaris-sparcv9");      platformStart+=(8);  platformEnd+=(8)
-temurinPlatforms+=("solaris-x64");          platformStart+=(8);  platformEnd+=(8)
 temurinPlatforms+=("windows-aarch64");      platformStart+=(21); platformEnd+=(99)
 temurinPlatforms+=("windows-x64");          platformStart+=(8);  platformEnd+=(99)
 temurinPlatforms+=("windows-x86-32");       platformStart+=(8);  platformEnd+=(17)
@@ -293,7 +291,7 @@ identifyFailedBuildsInTimerPipelines() {
     declare -a listOfBuildNums
     declare -a listOfBuildResults
 
-    shorterListOfBuilds=""
+    shorterListOfBuilds=","
 
     # Using this single-build tuple to ensure all of the build data lines up in the three arrays.
     sbTuple=("none" "none" "none")
@@ -302,20 +300,35 @@ identifyFailedBuildsInTimerPipelines() {
     for jsonEntry in $listOfPipelineBuilds
     do
       if [[ $jsonEntry =~ ^\"buildName\"\:.* ]]; then
-        sbTuple[0]=${jsonEntry}
-      elif [[ $jsonEntry =~ .*\"buildNum\"\.* ]]; then
-        sbTuple[1]=${jsonEntry}
+        sbTuple[0]="${jsonEntry:13:-1}"
+      elif [[ $jsonEntry =~ .*\"buildNum\".* ]]; then
+        sbTuple[1]="${jsonEntry:11}"
       elif [[ $jsonEntry =~ .*\"buildResult\".* ]]; then
-        sbTuple[2]=${jsonEntry}
+        sbTuple[2]="${jsonEntry:15:-1}"
       elif [[ $jsonEntry =~ \"_id\" ]]; then
         sbTuple=("none" "none" "none")
       fi
-      if [[ ! "${sbTuple[0]},${sbTuple[1]},${sbTuple[2]}" =~ none ]]; then
-        listOfBuildNames+=("${sbTuple[0]:13:-1}")
-        listOfBuildNums+=("${sbTuple[1]:11}")
-        listOfBuildResults+=("${sbTuple[2]:15:-1}")
+      if [[ ! "${sbTuple[0]},${sbTuple[1]}" =~ none ]]; then
+        listOfBuildNames+=("${sbTuple[0]}")
+        listOfBuildNums+=("${sbTuple[1]}")
+        if [[ "${sbTuple[2]}" == "none" ]]; then
+          # Fetch the build result from jenkins because trss doesn't have it yet.
+          jobNamePrefix=$(echo "${sbTuple[0]}" | cut -d- -f1)
+          jobName="${jobNamePrefix}/job/${sbTuple[0]}/${sbTuple[1]}"
+          buildData=$(wget -q -O - "https://ci.adoptium.net/job/build-scripts/job/jobs/job/${jobName}/api/xml")
+          if [[ "${buildData}" =~ \<result\>[A-Z]+\<\/result\> ]]; then
+            resultString="${buildData#*<result>}"
+            resultString="${resultString%</result>*}"
+            listOfBuildResults+=("${resultString}")
+          else
+            listOfBuildResults+=("UNKNOWN")
+          fi
+        else
+          listOfBuildResults+=("${sbTuple[2]}")
+        fi
+        
         shorterListOfBuilds+="${sbTuple[0]},"
-        sbTuple=("none" "none" "none")
+        sbTuple=("none" "none")
       fi
     done
 
@@ -328,7 +341,7 @@ identifyFailedBuildsInTimerPipelines() {
     triageThesePlatforms=","
     for p in "${!temurinPlatforms[@]}"
     do
-      if [[ $shorterListOfBuilds =~ .*\"buildName\"\:\"${jdkJenkinsJobVersion}\-${temurinPlatforms[p]}\-temurin\".* ]]; then
+      if [[ $shorterListOfBuilds =~ .*,${jdkJenkinsJobVersion}\-${temurinPlatforms[p]}\-temurin,.* ]]; then
         if [[ ${arrayOfAllJDKVersions[v]} -lt ${platformStart[p]} ]]; then
           errorLog "Error: Platform ${temurinPlatforms[p]} should not be built for ${jdkJenkinsJobVersion}. Will not triage."
           continue
@@ -372,6 +385,45 @@ identifyFailedBuildsInTimerPipelines() {
         fi
       fi
     done
+    
+    # Finally, if this is jdk8, we handle the latest Solaris builds seperately because their URL is unique to them.
+    if [[ "${jdkJenkinsJobVersion}" == "jdk8u" ]]; then
+      # Solaris x64
+      solarisJobURL="https://ci.adoptium.net/job/build-scripts/job/jobs/job/jdk8u/job/jdk8u-solaris-x64-temurin-simplepipe/lastCompletedBuild/"
+      solarisJobData=$(wget -q -O - "${solarisJobURL}api/xml")
+      if [[ "${solarisJobData}" =~ \<result\>[A-Z]+\<\/result\> ]]; then
+        solarisJobURL=$(wget -q -O - "${solarisJobURL}api/xml?xpath=/workflowRun/url")
+        solarisJobURL=${solarisJobURL:5:-7}
+        echo "Identified a Solaris x64 job. Checking status now. URL: ${solarisJobURL}"
+        if [[ ! "${solarisJobData}" =~ \<result\>(SUCCESS|UNSTABLE)\<\/result\> ]]; then
+          solarisJobURL=$(wget -q -O - "${solarisJobURL}api/xml?xpath=/workflowRun/url")
+          solarisJobURL=${solarisJobURL:5:-7}
+          echo "Identified a failed build for triage: ${solarisJobURL}"
+          arrayOfFailedJobs+=("${solarisJobURL}")
+        else
+          echo "Solaris x64 job did not fail."
+        fi
+      else
+        errorLog "Could not find a valid job for Solaris x64. URL: ${solarisJobURL}"
+      fi
+      
+      # Solaris sparcv9
+      solarisJobURL="https://ci.adoptium.net/job/build-scripts/job/jobs/job/jdk8u/job/jdk8u-solaris-sparcv9-temurin-simplepipe/lastCompletedBuild/"
+      solarisJobData=$(wget -q -O - "${solarisJobURL}api/xml")
+      if [[ "${solarisJobData}" =~ \<result\>[A-Z]+\<\/result\> ]]; then
+        solarisJobURL=$(wget -q -O - "${solarisJobURL}api/xml?xpath=/workflowRun/url")
+        solarisJobURL=${solarisJobURL:5:-7}
+        echo "Identified a Solaris sparcv9 job. Checking status now. URL: ${solarisJobURL}"
+        if [[ ! "${solarisJobData}" =~ \<result\>(SUCCESS|UNSTABLE)\<\/result\> ]]; then
+          echo "Identified a failed build for triage: ${solarisJobURL}"
+          arrayOfFailedJobs+=("${solarisJobURL}")
+        else
+          echo "Solaris sparcv9 job did not fail."
+        fi
+      else
+        errorLog "Could not find a valid job for Solaris sparcv9. URL: ${solarisJobURL}"
+      fi
+    fi
     echo "Build numbers found, and failures will be added to the array of builds to be triaged."
   done
 }
