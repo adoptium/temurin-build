d3c1cde18546fef50b5427da0ab05b0674d1e6b9
Adam Farley <adfarley@ibm.com>
2025-09-12 15:53:59 +0100

    Amending double quotes to enable looping on the wildcard matches
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- sbin/solaris/dotests.sh
+++ sbin/solaris/dotests.sh
@@ -72,7 +72,7 @@ fi
 PWD=`pwd`
 TEST_JDK_HOME=""
 JRE_IMAGE=""
-for FILE in "$PWD/jdk8u*"; do
+for FILE in "$PWD"/jdk8u*; do
   [ "`echo "$FILE" | grep -v jre`" ] && TEST_JDK_HOME="$FILE"
   [ "`echo "$FILE" | grep jre`" ] && JRE_IMAGE="$FILE"
 done
