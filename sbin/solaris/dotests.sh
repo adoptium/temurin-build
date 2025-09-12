890bcec39d223c11e9e016033f330aeac59a3663
Adam Farley <adfarley@ibm.com>
2025-09-12 14:27:34 +0100

    Correcting and/or exempting linter shellcheck code warnings
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- sbin/solaris/dotests.sh
+++ sbin/solaris/dotests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# shellcheck disable=SC2155,SC2153,SC2038,SC1091,SC2116,SC2086
+# shellcheck disable=SC2155,SC2153,SC2038,SC1091,SC2116,SC2086,SC2006
 # ********************************************************************************
 # Copyright (c) 2017, 2024 Contributors to the Eclipse Foundation
 #
@@ -12,7 +12,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # ********************************************************************************
-# shellcheck disable=SC2006
 #
 # dotests.sh
 # Designed for running via a proxy machine for the Temurin Solaris tests
@@ -24,7 +23,6 @@
 # ./dotests.sh sanity openjdk
 #
 
-if [ `uname -m` = i86pc ]; then ADOPTARCH=x64; else ADOPTARCH=sparcv9; fi
 # Check for Xvfb on display :5
 XVFB5=`ps -fu vagrant | grep 'Xvfb :5' | grep -v grep | wc -l`
    echo XVFB5 = $XVFB5
@@ -43,7 +41,7 @@ if [ "$3" = "usecache" ]; then
 else
   rm -rf aqa-tests
   git clone https://github.com/adoptium/aqa-tests
-  cd aqa-tests
+  cd aqa-tests || exit 1
   if [ -z "${UPSTREAM_JOBLINK}" ]; then
     # Jenkins simpletest job will copy the artifacts to this location
     if [ ! -r "$PWD/build_artifacts/filenames.txt" ]; then
@@ -61,27 +59,36 @@ else
   gzip -cd "$JDK_TARBALL_NAME" | tar xpf -
   echo Downloading and extracting JRE tarball ... Required for special.openjdk jdk_math_jre_0 target
   JRE_TARBALL_NAME="`echo $JDK_TARBALL_NAME | sed s/jdk/jre/`"
-  if [ "$1" = "special" -a "$2" = "openjdk" ]; then
-    [ ! -z "${UPSTREAM_JOBLINK}" ] && curl -O "${UPSTREAM_JOBLINK}/artifact/workspace/target/$JRE_TARBALL_NAME" || exit 1
-    gzip -cd "$JRE_TARBALL_NAME" | tar xpf -
-  fi
+  if [ "$1" = "special"]; then
+    if [ "$2" = "openjdk" ]; then
+      if [ ! -z "${UPSTREAM_JOBLINK}" ]; then
+        curl -O "${UPSTREAM_JOBLINK}/artifact/workspace/target/$JRE_TARBALL_NAME" || exit 1
+      fi
+      gzip -cd "$JRE_TARBALL_NAME" | tar xpf -
+    fi
+  fi  
 fi
 PWD=`pwd`
-TEST_JDK_HOME=`ls -1d $PWD/jdk8u* | grep -v jre`
-JRE_IMAGE=`ls -1d $PWD/jdk8u* | grep jre`
+TEST_JDK_HOME=""
+JRE_IMAGE=""
+for FILE in $PWD/jdk8u*; do
+  [ `echo "$FILE" | grep -v jre` ] && TEST_JDK_HOME="$FILE"
+  [ `echo "$FILE" | grep jre` ] && JRE_IMAGE="$FILE"
+done
 env
 # TODO: Check if this actually exists
-[ -z "$TEST_JDK_HOME" ] && echo Could not resolve TEST_JDK_HOME - aborting && exit 1
+[ -z "$TEST_JDK_HOME" ] && echo "Could not resolve TEST_JDK_HOME - aborting" && exit 1
 echo TEST_JDK_HOME=$TEST_JDK_HOME
 "$TEST_JDK_HOME/bin/java" -version || exit 1
 BUILD_LIST=$2
 if [ "$BUILD_LIST" = "system" ]; then
-  mkdir -p `pwd`/systemtest_prereqs/mauve # systemtest_preqeqs not created til compile phase
-  curl -o `pwd`/systemtest_prereqs/mauve/mauve.jar \
+  mkdir -p "`pwd`/systemtest_prereqs/mauve" # systemtest_preqeqs not created til compile phase
+  curl -o "`pwd`/systemtest_prereqs/mauve/mauve.jar" \
 	https://ci.adoptium.net/job/systemtest.getDependency/lastSuccessfulBuild/artifact/systemtest_prereqs/mauve/mauve.jar
 fi
 GET_SH_PARAMS=""
 if [ "$1" = "smoke" ]; then
+  # shellcheck disable=SC2121
   set extended functional
   GET_SH_PARAMS="--vendor_repos https://github.com/adoptium/temurin-build --vendor_branches master --vendor_dirs /test/functional"
   BUILD_LIST=functional/buildAndPackage
@@ -90,9 +97,9 @@ fi
 PATH=/usr/local/bin:/opt/csw/bin:`echo $PATH | sed 's,/usr/xpg4/bin,,g'`
 export TEST_JDK_HOME BUILD_LIST PATH JRE_IMAGE
 [ "$3" != "usecache" ] && ./get.sh ${GET_SH_PARAMS}
-cd TKG
+cd TKG || exit 1
 (echo VENDOR OPTIONS = $VENDOR_TEST_REPOS / $VENDOR_TEST_DIRS / $VENDOR_TEST_BRANCHES)
 gmake compile
-echo SXAEC: Running gmake _$1.$2 from `pwd`
+echo SXAEC: Running gmake _$1.$2 from "`pwd`"
 DISPLAY=:5; export DISPLAY
 gmake _$1.$2 2>&1 | tee $1.$2.$$.log
