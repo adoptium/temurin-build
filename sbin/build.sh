052c1c981992447543093f32fa7d9410749c1526
Adam Farley <adfarley@ibm.com>
2025-11-14 14:46:04 +0000

    Refactoring unbound variable handling
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- sbin/build.sh
+++ sbin/build.sh
@@ -477,7 +477,8 @@ getOpenJdkVersion() {
     # Now we check if the version in the code is later than the version we have so far.
     # This prevents an issue where the git repo tags are not updated to match the hard-coded version string.
     # If SCM_REF is specified, we don't do this check as SCM_REF gets priority.
-    if [ -z "${BUILD_CONFIG[SCM_REF]+x}" ]; then
+    tag_ref="SCM_REF"
+    if [ -z "${BUILD_CONFIG[$tag_ref]+x}" ]; then
       version=$(compareToOpenJDKFileVersion "$version")
     fi
   fi
