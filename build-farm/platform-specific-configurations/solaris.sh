fcff1612f96792ffca9f039e7bcee1589f140b5d
Jiri Vanek <jvanek@redhat.com>
2025-12-15 16:44:08 +0100

    Remove obsolated --with-freetype=/usr/local/ from solaris build

--- build-farm/platform-specific-configurations/solaris.sh
+++ build-farm/platform-specific-configurations/solaris.sh
@@ -24,11 +24,10 @@ if [ "${ARCHITECTURE}" == "x64" ]; then
   export MEMORY=4000
 elif [ "${ARCHITECTURE}" == "sparcv9" ]; then
   export CUPS="--with-cups=/opt/csw/lib/ --with-cups-include=/usr/local/cups-1.5.4-src"
-  export FREETYPE="--with-freetype=/usr/local/"
   export MEMORY=16000
 fi
 
-export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} ${CUPS} ${FREETYPE} --with-memory-size=${MEMORY}"
+export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} ${CUPS} --with-memory-size=${MEMORY}"
 # /usr/sfw/bin required for OpenSSL (build#2265)
 export PATH=/opt/solarisstudio12.3/bin/:/opt/csw/bin/:/usr/ccs/bin:$PATH:/usr/sfw/bin
 
