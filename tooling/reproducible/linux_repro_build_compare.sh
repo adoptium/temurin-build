1b742b88c0edd87d02f65f86f33e6c135545d7b9
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-01-09 14:54:28 +0000

    Fix reproducible build tests
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- tooling/reproducible/linux_repro_build_compare.sh
+++ tooling/reproducible/linux_repro_build_compare.sh
@@ -206,7 +206,8 @@ else
 fi
 
 echo "Rebuild args for makejdk_any_platform.sh are: $TEMURIN_BUILD_ARGS"
-echo " cd temurin-build && ./makejdk-any-platform.sh $TEMURIN_BUILD_ARGS > build.log 2>&1" | sh
+#echo " cd temurin-build && ./makejdk-any-platform.sh $TEMURIN_BUILD_ARGS > build.log 2>&1" | sh
+echo " cd temurin-build && ./makejdk-any-platform.sh $TEMURIN_BUILD_ARGS" | sh
 
 echo Comparing ...
 mkdir tarJDK
