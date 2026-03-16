d12b57f527073a3de9fbdea9d330e4c70eb67f9d
Adam Farley <adfarley@ibm.com>
2026-03-16 13:34:42 +0000

    Updating the freetype version expected in build sboms
    JDK26 GA has freetype 2.13.3, and 26.0.1 will have 2.14.2, so I'm
    allowing either value to be valid for that JDK, until we are safely
    past the 26 ga release.
    
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- tooling/validateSBOMcontent.sh
+++ tooling/validateSBOMcontent.sh
@@ -35,7 +35,8 @@ COMPILER=$(jq '.components[0].properties[] | select(.name|test("Build Tools Summ
 SCM_REF=$( jq '.components[0].properties[]  | select(.name|test("SCM Ref"))  | .value'   "$SBOMFILE" | tr -d \")
 
 EXPECTED_COMPILER="gcc (GNU Compiler Collection)"
-EXPECTED_FREETYPE=2.13.3 # Bundled version
+EXPECTED_FREETYPE=2.14.2 # Bundled version for jdk11+
+[ "${MAJORVERSION}" = "8" ] && EXPECTED_FREETYPE=2.13.3 # Bundled version for jdk8
 EXPECTED_GLIBC=""
 EXPECTED_GCC=""
 # [ "${MAJORVERSION}" = "17" ] && EXPECTED_GCC=10.3.0
@@ -102,13 +103,16 @@ fi
 echo "BOOTJDK is ${BOOTJDK}"
 [ "${COMPILER}"   != "$EXPECTED_COMPILER" ] && echo "ERROR: Compiler version not ${EXPECTED_COMPILER} (SBOM has ${COMPILER})"   && RC=1
 [ "${ALSA}"       != "$EXPECTED_ALSA"     ] && echo "NOTE: ALSA version not ${EXPECTED_ALSA} (SBOM has ${ALSA}) - ignoring because ALSA version is determined by devkit now"  # && RC=1
-# Freetype versions are inconsistent at present - see build#3484
-#[ "${FREETYPE}"   != "$EXPECTED_FREETYPE" ] && echo "ERROR: FreeType version not ${EXPECTED_FREETYPE} (SBOM has ${FREETYPE})"   && RC=1
 
 # shellcheck disable=SC2086
-[ "${FREETYPE}"   != "$EXPECTED_FREETYPE" ] && echo "ERROR: FreeType version not ${EXPECTED_FREETYPE} (SBOM has ${FREETYPE})"   && RC=1
+[ "${MAJORVERSION}" != "26" ] && [ "${FREETYPE}"   != "$EXPECTED_FREETYPE" ]  && echo "ERROR: FreeType version not ${EXPECTED_FREETYPE} (SBOM has ${FREETYPE})"   && RC=1
+
+# Temporary entry. This one line of code is to be removed after the jdk26 ga release.
+# At the same time, remove this part of the line of code above: [ "${MAJORVERSION}" != "26" ] &&
+[ "${MAJORVERSION}" = "26" ] && [ "${FREETYPE}" != "$EXPECTED_FREETYPE" ] && [[ "$FREETYPE" != "2.13.3" ]] && echo "ERROR: FreeType version not ${EXPECTED_FREETYPE} or 2.13.3 (SBOM has ${FREETYPE})" && RC=1
 
 echo "FREETYPE is ${FREETYPE}"
+
 # shellcheck disable=SC3037
 echo "Checking for JDK source SHA validity..."
 GITURL=$(jq '.components[].properties[] | select(.name|test("OpenJDK Source Commit")) | .value' "$1" | tr -d \" | uniq)
