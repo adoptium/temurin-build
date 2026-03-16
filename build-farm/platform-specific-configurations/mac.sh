77367334f3867c3753fe910763b379bef6f58afd
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-03-16 16:10:12 +0000

    Enable Mac platform -Wl,-deterministic option for Mac build-farm builds with Xcode >= 15
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -88,6 +88,11 @@ fi
 echo "[WARNING] You may be asked for your su user password, attempting to switch Xcode version to ${XCODE_SWITCH_PATH}"
 sudo xcode-select --switch "${XCODE_SWITCH_PATH}"
 
+# If using Xcode >= 15, then Apple have introduced linker option -deterministic, to enable reproducible dylib's
+if [ $(xcodebuild -version | grep "Xcode" | awk '{print $2}' | cut -d. -f1) -ge 15 ]; then
+  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-ldflags=-Wl,-deterministic"
+fi
+
 # No MacOS builds available of OpenJDK 7, OpenJDK 8 can boot itself just fine.
 if [ "${JDK_BOOT_VERSION}" == "7" ]; then
   echo "No jdk7 boot JDK available on MacOS using jdk8"
