9e6fb25b9c9acce8e10512b74905e9d34681d06e
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-03-18 09:27:26 +0000

    Enable Mac platform -Wl,-reproducible option for Mac build-farm builds with Xcode >= 15
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- build-farm/platform-specific-configurations/mac.sh
+++ build-farm/platform-specific-configurations/mac.sh
@@ -105,9 +105,9 @@ fi
 echo "[WARNING] You may be asked for your su user password, attempting to switch Xcode version to ${XCODE_SWITCH_PATH}"
 sudo xcode-select --switch "${XCODE_SWITCH_PATH}"
 
-# If using Xcode >= 15 for jdk-21+, then Apple have introduced linker option -deterministic, to enable reproducible dylib's
+# If using Xcode >= 15 for jdk-21+, then Apple have introduced linker option -reproducible, to enable reproducible dylib's
 if [ "$JAVA_FEATURE_VERSION" -ge 21 ] && [ "$(xcodebuild -version | grep 'Xcode' | awk '{print $2}' | cut -d. -f1)" -ge 15 ]; then
-  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-ldflags=-Wl,-deterministic"
+  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-extra-ldflags=-Wl,-reproducible"
 fi
 
 # No MacOS builds available of OpenJDK 7, OpenJDK 8 can boot itself just fine.
