55014ac18c33583fa9128c9ada3d905554276cb1
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2026-05-18 10:10:09 +0100

    windows reproducible verification script fails with signtool on non-admin user (#4460)
    * pfx permissions
    
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>
    
    * Fix windows self cert signing on non-Admin
    
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>
    
    ---------
    
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- tooling/reproducible/repro_common.sh
+++ tooling/reproducible/repro_common.sh
@@ -483,9 +483,11 @@ function tempSign() {
     # is only used for generating a temporary dummy signature required for
     # the comparison and not used for validating anything
     # nosemgrep
-    openssl req -x509 -quiet -newkey rsa:4096 -sha256 -days 3650 -passout pass:test -keyout $selfCert.key -out $selfCert.crt -subj "/CN=example.com" -addext "subjectAltName=DNS:example.com,DNS:*.example.com,IP:10.0.0.1"
+    openssl req -x509 -quiet -newkey rsa:4096 -sha256 -days 3650 -passout pass:test -keyout $selfCert.key -out $selfCert.crt -subj "/CN=example.com" -addext "extendedKeyUsage=codeSigning" -addext "keyUsage=digitalSignature" -addext "subjectAltName=DNS:example.com,DNS:*.example.com,IP:10.0.0.1"
+
     # nosemgrep
     openssl pkcs12 -export -passout pass:test -passin pass:test -out $selfCert.pfx -inkey $selfCert.key -in $selfCert.crt
+
     FILES=$(find "${JDK_DIR}" -type f -name '*.exe' -o -name '*.dll')
     for f in $FILES
      do
